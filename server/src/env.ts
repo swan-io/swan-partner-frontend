e0bfd577075c8f9870d712e41433752a05c25478
Mathieu Acthernoene <zoontek@gmail.com>
2023-03-26 21:10:19 +0200

    Add buffer validator

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,5 +1,7 @@
 import { oneOf, string, validate, Validator } from "valienv";
 
+const buffer: Validator<Buffer> = value => Buffer.from(value, "hex");
+
 const url: Validator<string> = value => {
   try {
     new URL(value);
@@ -7,22 +9,19 @@ const url: Validator<string> = value => {
   } catch {} // eslint-disable-line no-empty
 };
 
-export const env = {
-  ...validate({
-    env: process.env,
-    validators: {
-      NODE_ENV: oneOf("development", "production", "test"),
-      LOG_LEVEL: oneOf("fatal", "error", "warn", "info", "debug", "trace", "silent"),
-      PARTNER_API_URL: url,
-      UNAUTHENTICATED_API_URL: url,
-      OAUTH_SERVER_URL: url,
-      OAUTH_CLIENT_ID: string,
-      OAUTH_CLIENT_SECRET: string,
-      COOKIE_KEY: string,
+export const env = validate({
+  env: process.env,
+  validators: {
+    NODE_ENV: oneOf("development", "production", "test"),
+    LOG_LEVEL: oneOf("fatal", "error", "warn", "info", "debug", "trace", "silent"),
+    PARTNER_API_URL: url,
+    UNAUTHENTICATED_API_URL: url,
+    OAUTH_SERVER_URL: url,
+    OAUTH_CLIENT_ID: string,
+    OAUTH_CLIENT_SECRET: string,
+    COOKIE_KEY: buffer,
 
-      BANKING_URL: url,
-      ONBOARDING_URL: url,
-    },
-  }),
-  COOKIE_KEY: Buffer.from(process.env.COOKIE_KEY as string, "hex"),
-};
+    BANKING_URL: url,
+    ONBOARDING_URL: url,
+  },
+});

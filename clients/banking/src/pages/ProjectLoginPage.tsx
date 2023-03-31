d6308d513ffb350e1cc3c897d2a712c026e9f6e1
Mathieu Acthernoene <zoontek@gmail.com>
2023-03-31 18:25:42 +0200

    Use lighter shadow

--- clients/banking/src/pages/ProjectLoginPage.tsx
+++ clients/banking/src/pages/ProjectLoginPage.tsx
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
     maxWidth: 360,
     overflow: "hidden",
     width: "100%",
-    boxShadow: shadows.tileHover,
+    boxShadow: shadows.tile,
   },
   gradient: {
     height: 208,

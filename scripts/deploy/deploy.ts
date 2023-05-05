d8b388a4eaf30dddfb2a14f70bfe909bac1e7f17
bloodyowl <bloodyowl@icloud.com>
2023-05-05 16:48:30 +0200

    tmp: try working branch

--- scripts/deploy/deploy.ts
+++ scripts/deploy/deploy.ts
@@ -14,7 +14,7 @@ assert(process.env.DEPLOY_ENVIRONMENT);
 assert(process.env.DEPLOY_APP_NAME);
 
 execSync(
-  `cd ${tmp} && git clone --single-branch --branch master https://projects:${process.env.DEPLOY_SWAN_TOKEN}@${process.env.DEPLOY_SWAN_REPOSITORY} ${repoName}`,
+  `cd ${tmp} && git clone --single-branch --branch OPS-1464/deploy-new-frontend-apps https://projects:${process.env.DEPLOY_SWAN_TOKEN}@${process.env.DEPLOY_SWAN_REPOSITORY} ${repoName}`,
 );
 
 const file = fs.readFileSync(
@@ -44,4 +44,6 @@ execSync(
   `cd ${tmp}/${repoName} && git -am "Update with tag: ${process.env.DEPLOY_ENVIRONMENT}-${process.env.TAG}, image(s): ${process.env.DEPLOY_APP_NAME}"`,
 );
 
-execSync(`cd ${tmp}/${repoName} && git pull --rebase origin master && git push origin master`);
+execSync(
+  `cd ${tmp}/${repoName} && git pull --rebase origin OPS-1464/deploy-new-frontend-apps && git push origin OPS-1464/deploy-new-frontend-apps`,
+);

594a3323b0571135b802982331dcb0488eb9baf9
Mathieu Acthernoene <zoontek@gmail.com>
2023-05-30 22:00:33 +0200

    Indent generated env following prettier rules

--- types/env/index.d.ts
+++ types/env/index.d.ts
@@ -1,12 +1,11 @@
-
-  declare const __env: {
-    // Server provided
-    VERSION: string;
-    SWAN_PROJECT_ID?: string;
-    SWAN_ENVIRONMENT: "SANDBOX" | "LIVE";
-    ACCOUNT_MEMBERSHIP_INVITATION_MODE: "LINK" | "EMAIL";
-    BANKING_URL: string;
-    IS_SWAN_MODE: boolean;
-    // Client
-    CLIENT_GOOGLE_MAPS_API_KEY: string;
-  }
+declare const __env: {
+  // Server provided
+  VERSION: string;
+  SWAN_PROJECT_ID?: string;
+  SWAN_ENVIRONMENT: "SANDBOX" | "LIVE";
+  ACCOUNT_MEMBERSHIP_INVITATION_MODE: "LINK" | "EMAIL";
+  BANKING_URL: string;
+  IS_SWAN_MODE: boolean;
+  // Client
+  CLIENT_GOOGLE_MAPS_API_KEY: string;
+};

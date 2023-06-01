befb141537a4b2f4115a8b611aa0e074662a8878
bloodyowl <bloodyowl@icloud.com>
2023-06-01 11:47:50 +0200

    Try new instrumentation

--- server/src/tracing.cjs
+++ server/src/tracing.cjs
@@ -1,25 +1,37 @@
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const { FastifyInstrumentation } = require("@opentelemetry/instrumentation-fastify");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { PinoInstrumentation } = require("@opentelemetry/instrumentation-pino");
+const { JaegerPropagator } = require("@opentelemetry/propagator-jaeger");
 const { Resource } = require("@opentelemetry/resources");
-const opentelemetry = require("@opentelemetry/sdk-node");
-const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const process = require("process");
 
 if (process.env.TRACING_SERVICE_NAME != null) {
-  const traceExporter = new ConsoleSpanExporter();
-  const sdk = new opentelemetry.NodeSDK({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: process.env.TRACING_SERVICE_NAME,
-    }),
-    traceExporter,
-    instrumentations: [getNodeAutoInstrumentations()],
+  const provider = new NodeTracerProvider({
+    resource: Resource.default().merge(
+      new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: process.env.TRACING_SERVICE_NAME,
+      }),
+    ),
   });
-  sdk.start();
-  process.on("SIGTERM", () => {
-    sdk
-      .shutdown()
-      .then(() => console.log("Tracing terminated"))
-      .catch(error => console.log("Error terminating tracing", error))
-      .finally(() => process.exit(0));
+
+  const jaegerPropagator = new JaegerPropagator();
+  const jaegerExporter = new JaegerExporter();
+  const batchSpanProcessor = new BatchSpanProcessor(jaegerExporter);
+
+  provider.addSpanProcessor(batchSpanProcessor);
+  provider.register({ propagator: jaegerPropagator });
+
+  registerInstrumentations({
+    instrumentations: [
+      new PinoInstrumentation(),
+      new HttpInstrumentation({
+        ignoreIncomingPaths: [/\/health/],
+      }),
+      new FastifyInstrumentation(),
+    ],
   });
 }

4a5f538e305a15d7eac48bc3db90623e7c99a0fe
Mathieu Acthernoene <zoontek@gmail.com>
2023-06-07 18:05:20 +0200

    re-order fns

--- tests/utils/functions.ts
+++ tests/utils/functions.ts
@@ -1,15 +1,23 @@
 import fs from "node:fs/promises";
 import { testResultsDir } from "../../playwright.config";
 
-export const seconds = (value: number) => value * 1000;
-export const minutes = (value: number) => value * seconds(60);
-
 export const log = {
   success: (text: string) => console.log(`[${new Date().toISOString()}] ✓ ${text}`),
   info: (text: string) => console.info(`[${new Date().toISOString()}] ℹ ${text}`),
   debug: (text: string) => console.debug(`[${new Date().toISOString()}] ℹ ${text}`),
   error: (text: string) => console.error(`[${new Date().toISOString()}] ✕ ${text}`),
 };
+
+export const resolveAfter = (ms: number) =>
+  new Promise<void>(resolve => {
+    setTimeout(resolve, ms);
+  });
+
+export const createTestResultsDir = () => fs.mkdir(testResultsDir);
+export const deleteTestResultsDir = () => fs.rm(testResultsDir, { force: true, recursive: true });
+
+export const seconds = (value: number) => value * 1000;
+export const minutes = (value: number) => value * seconds(60);
 
 export function assertIsDefined<T>(value: T): asserts value is NonNullable<T> {
   if (value == null) {
@@ -25,11 +33,3 @@ export function assertTypename<T extends string, E extends T>(
     throw new Error(`__typename is not "${expected}":\n${JSON.stringify(input, null, 2)}`);
   }
 }
-
-export const resolveAfter = (ms: number) =>
-  new Promise<void>(resolve => {
-    setTimeout(resolve, ms);
-  });
-
-export const createTestResultsDir = () => fs.mkdir(testResultsDir);
-export const deleteTestResultsDir = () => fs.rm(testResultsDir, { force: true, recursive: true });

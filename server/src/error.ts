9282fb2528f9f3aa0d6172c490e94bdb2ca213cb
Mathieu Acthernoene <zoontek@gmail.com>
2023-06-20 17:11:55 +0200

    Improve matching

--- server/src/error.ts
+++ server/src/error.ts
@@ -5,18 +5,20 @@ import fs from "node:fs";
 import { Http2SecureServer, Http2ServerRequest, Http2ServerResponse } from "node:http2";
 import url from "node:url";
 import path from "pathe";
-import { P, match } from "ts-pattern";
+import { match } from "ts-pattern";
 
 const dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const errorTemplate = fs.readFileSync(path.join(dirname, "views", "error.html"), "utf-8");
 const authErrorTemplate = fs.readFileSync(path.join(dirname, "views", "auth-error.html"), "utf-8");
 
-const getAcceptType = (accept: Accepts) =>
-  match(accept.type(["json", "html"]))
-    .with(P.string, value => value)
-    .with(P.array(P.string), value => value[0])
-    .otherwise(() => undefined);
+const getAcceptType = (accept: Accepts): "html" | "json" => {
+  const type = accept.type(["html", "json"]);
+
+  return match(Array.isArray(type) ? type[0] : type)
+    .with("json", value => value)
+    .otherwise(() => "html");
+};
 
 export const replyWithError = (
   app: FastifyInstance<Http2SecureServer, Http2ServerRequest, Http2ServerResponse>,

b38e1d18999820da1bf0959826848abe2d4e918e
Mathieu Acthernoene <zoontek@gmail.com>
2023-06-26 15:55:24 +0200

    Make test fail

--- tests/1-onboarding.spec.ts
+++ tests/1-onboarding.spec.ts
@@ -1,7 +1,9 @@
 import { test } from "@playwright/test";
 import { env } from "./utils/env";
+import { waitForText } from "./utils/selectors";
 
 test("Onboarding", async ({ page }) => {
   await page.goto(`${env.ONBOARDING_URL}/onboarding/individual/start`);
+  await waitForText(page, "NOT IN THIS PAGE");
   // await page.pause();
 });

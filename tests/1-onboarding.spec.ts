49f5aa6a95ebc122f64d29dce537d41b10bda975
Mathieu Acthernoene <zoontek@gmail.com>
2023-06-26 16:01:14 +0200

    Run conditionally

--- tests/1-onboarding.spec.ts
+++ tests/1-onboarding.spec.ts
@@ -1,9 +1,7 @@
 import { test } from "@playwright/test";
 import { env } from "./utils/env";
-import { waitForText } from "./utils/selectors";
 
 test("Onboarding", async ({ page }) => {
   await page.goto(`${env.ONBOARDING_URL}/onboarding/individual/start`);
-  await waitForText(page, "NOT IN THIS PAGE");
   // await page.pause();
 });

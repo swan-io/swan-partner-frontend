7ef140f5281145814ee9c43817ad835fa181d98d
Mathieu Acthernoene <zoontek@gmail.com>
2023-06-27 11:41:43 +0200

    Rename test

--- tests/0-onboarding.spec.ts
+++ tests/0-onboarding.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test";
 import { env } from "./utils/env";
 
-test("My awesome test", async ({ page }) => {
+test("Onboarding", async ({ page }) => {
   await page.goto(`${env.ONBOARDING_URL}/onboarding/individual/start`);
   await page.pause();
 });

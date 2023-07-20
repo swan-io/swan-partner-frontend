388c041c92f8fa8036edf91eeca9876885e15da2
Nicolas Comont <contact@nicolasc.eu>
2023-07-20 14:57:02 +0200

    feat(e2e): add exact param to waitForText and clickOnText (#269)

--- tests/utils/selectors.ts
+++ tests/utils/selectors.ts
@@ -1,30 +1,43 @@
 import { Locator, Page } from "@playwright/test";
+import { Merge } from "type-fest";
 
 type Parent = Page | Locator;
 
-type GetByRoleOptions = Parameters<Page["getByRole"]>[1];
-type GetByTextOptions = Parameters<Page["getByText"]>[1];
-type ClickOptions = Parameters<Locator["click"]>[0];
-type WaitForOptions = Parameters<Locator["waitFor"]>[0];
+type GetByTextOptions = { exact?: boolean };
+type GetByRoleOptions = Merge<Omit<Parameters<Page["getByRole"]>[1], "name">, GetByTextOptions>;
+type ClickOptions = Merge<Parameters<Locator["click"]>[0], GetByTextOptions>;
+type WaitForOptions = Merge<Parameters<Locator["waitFor"]>[0], GetByTextOptions>;
 
 // Selectors
 
 export const getButtonByName = (
   parent: Parent,
   name: string | RegExp,
-  options?: Omit<GetByRoleOptions, "name">,
-) => parent.getByRole("button", { exact: true, ...options, name });
+  { exact = typeof name === "string", ...options }: GetByRoleOptions = {},
+) => parent.getByRole("button", { exact, ...options, name });
 
-export const getByText = (parent: Parent, text: string | RegExp, options?: GetByTextOptions) =>
-  parent.getByText(text, { exact: true, ...options });
+export const getByText = (
+  parent: Parent,
+  text: string | RegExp,
+  { exact = typeof text === "string", ...options }: GetByTextOptions = {},
+) => parent.getByText(text, { exact, ...options });
 
 // Actions
 
-export const clickOnButton = (parent: Parent, name: string | RegExp, options?: ClickOptions) =>
-  getButtonByName(parent, name).click(options);
+export const clickOnButton = (
+  parent: Parent,
+  name: string | RegExp,
+  { exact, ...options }: ClickOptions = {},
+) => getButtonByName(parent, name, { exact }).click(options);
 
-export const clickOnText = (parent: Parent, text: string | RegExp, options?: ClickOptions) =>
-  getByText(parent, text).click(options);
+export const clickOnText = (
+  parent: Parent,
+  text: string | RegExp,
+  { exact, ...options }: ClickOptions = {},
+) => getByText(parent, text, { exact }).click(options);
 
-export const waitForText = (parent: Parent, text: string | RegExp, options?: WaitForOptions) =>
-  getByText(parent, text).waitFor(options);
+export const waitForText = (
+  parent: Parent,
+  text: string | RegExp,
+  { exact, ...options }: WaitForOptions = {},
+) => getByText(parent, text, { exact }).waitFor(options);

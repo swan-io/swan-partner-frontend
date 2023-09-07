f347d6e001b791ec76423f1001c2ed1831cc576b
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-09-07 16:51:32 +0200

    update env var

--- clients/onboarding/src/pages/company/OnboardingCompanyOwnership.tsx
+++ clients/onboarding/src/pages/company/OnboardingCompanyOwnership.tsx
@@ -624,7 +624,7 @@ export const OnboardingCompanyOwnership = ({
       >
         <BeneficiaryForm
           ref={beneficiaryFormRef}
-          googleMapApiKey={__env.CLIENT_GOOGLE_MAPS_API_KEY}
+          googleMapApiKey={__env.CLIENT_PLACEKIT_API_KEY}
           initialState={match(pageState)
             .with({ type: "edit" }, ({ ubo }) => ubo)
             .otherwise(() => undefined)}

d30071847532f2ce4aa930e8c60d7fe33ff6e8e1
bloodyowl <bloodyowl@icloud.com>
2023-09-22 11:23:40 +0200

    Use actions instead of octokit

--- scripts/changelog/getChangelog.ts
+++ scripts/changelog/getChangelog.ts
@@ -1,9 +1,17 @@
-import { Octokit } from "@octokit/rest";
-import slacifyMarkdown from "slackify-markdown"
+import core from "@actions/core"
+import github from "@actions/github"
+import slackifyMarkdown from "slackify-markdown"
 
-const octokit = new Octokit({
-	auth: process.env.GITHUB_TOKEN
-});
+if (process.env.GITHUB_TOKEN == null) {
+	process.exit(0)
+}
+
+const context = github.context;
+// Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+// Get owner and repo from context of payload that triggered the action
+const { owner, repo } = context.repo;
 
 const tag = process.env.RELEASE_TAG?.replace("prod-", "")
 
@@ -12,13 +20,18 @@ if (tag == null) {
 }
 
 const main = async () => {
-	const release = await octokit.repos.getReleaseByTag({ owner: "swan-io", repo: "swan-partner-frontend", tag })
+	const release = await octokit.rest.repos.getReleaseByTag({
+		owner,
+		repo,
+		tag
+	})
 
 	const body = release.data.body
 	if (body == null) {
 		process.exit(1)
 	}
-	process.stdout.write(slacifyMarkdown(body).replace("*What's Changed*\n\n", ""))
+
+	core.setOutput("body", slackifyMarkdown(body).replace("*What's Changed*\n\n", ""))
 }
 
 void main()

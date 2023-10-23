6487da416580e3f81c538cf3813da766e4a607bc
Mathieu Acthernoene <zoontek@gmail.com>
2023-10-23 15:18:32 +0200

    Simplify code

--- clients/banking/src/utils/projectId.ts
+++ clients/banking/src/utils/projectId.ts
@@ -7,22 +7,13 @@ export const projectConfiguration = Result.fromExecution(() => {
     throw new Error();
   }
 })
-  .map(
-    projectId =>
-      ({
-        mode: "SingleProject",
-        projectId,
-      }) as const,
-  )
+  .map(projectId => ({ mode: "SingleProject", projectId }) as const)
   .flatMapError(() => {
     const [, projects, projectId] = location.pathname.split("/");
-    if (projects === "projects" && typeof projectId === "string") {
-      return Result.Ok({
-        mode: "MultiProject",
-        projectId,
-      } as const);
-    }
-    return Result.Error(new Error("No project specified"));
+
+    return projects === "projects" && typeof projectId === "string"
+      ? Result.Ok({ mode: "MultiProject", projectId } as const)
+      : Result.Error(new Error("No project specified"));
   })
   .toOption()
   .flatMap(nullable => Option.fromNullable(nullable));

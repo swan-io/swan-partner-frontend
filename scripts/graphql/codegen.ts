4699abde528f1f1935f9db2bbd13e0ec50f819a6
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-10-24 18:03:33 +0200

    Add payment subdomain

--- scripts/graphql/codegen.ts
+++ scripts/graphql/codegen.ts
@@ -103,6 +103,15 @@ const config: CodegenConfig = {
       documentTransforms: [{ transform: addTypenames }],
     },
 
+    [file("../../clients/payment/src/graphql/introspection.json")]: {
+      schema: file("./dist/unauthenticated-schema.gql"),
+      plugins: ["introspection"],
+      config: { descriptions: false },
+      hooks: {
+        afterOneFileWrite: "yarn tsx scripts/graphql/cleanIntrospectionSchema.ts",
+      },
+    },
+
     [file("../../clients/onboarding/src/graphql/unauthenticated.ts")]: {
       documents: file("../../clients/onboarding/src/graphql/unauthenticated.gql"),
       schema: file("./dist/unauthenticated-schema.gql"),

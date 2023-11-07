31ed951acd4ac8fb153ab2f0d2a554c325d55607
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-11-07 17:54:16 +0100

    update package json, add form

--- clients/payment/src/App.tsx
+++ clients/payment/src/App.tsx
@@ -1,31 +1,18 @@
-import { Box } from "@swan-io/lake/src/components/Box";
 import { ErrorBoundary } from "@swan-io/lake/src/components/ErrorBoundary";
-import { LakeButton } from "@swan-io/lake/src/components/LakeButton";
-import { LakeLabel } from "@swan-io/lake/src/components/LakeLabel";
-import { LakeText } from "@swan-io/lake/src/components/LakeText";
-import { LakeTextInput } from "@swan-io/lake/src/components/LakeTextInput";
 import { LoadingView } from "@swan-io/lake/src/components/LoadingView";
-import { SegmentedControl } from "@swan-io/lake/src/components/SegmentedControl";
-import { backgroundColor, colors } from "@swan-io/lake/src/constants/design";
-import { Suspense, useState } from "react";
-import { ScrollView } from "react-native";
+import { colors } from "@swan-io/lake/src/constants/design";
+import { Suspense } from "react";
 import { P, match } from "ts-pattern";
 import { Provider as ClientProvider } from "urql";
 import { ErrorView } from "./components/ErrorView";
-import { NotFoundPage } from "./components/pages/NotFoundPage";
-import { t } from "./utils/i18n";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { PaymentPage } from "./pages/PaymentPage";
 import { logFrontendError } from "./utils/logger";
 import { Router } from "./utils/routes";
 import { unauthenticatedClient } from "./utils/urql";
 
-const items = [{ id: "sdd", name: "SEPA Direct Debit" }] as const;
-
-type ItemId = (typeof items)[number]["id"];
-
 export const App = () => {
-  const route = Router.useRoute(["PaymentLinkArea"]);
-
-  const [selected, setSelected] = useState<ItemId>(items[0].id);
+  const route = Router.useRoute(["PaymentLink"]);
 
   return (
     <ErrorBoundary
@@ -33,61 +20,10 @@ export const App = () => {
       onError={error => logFrontendError(error)}
       fallback={({ error }) => <ErrorView error={error} />}
     >
-      <Suspense fallback={<LoadingView color={colors.gray[50]} />}>
+      <Suspense fallback={<LoadingView color={colors.gray[100]} />}>
         <ClientProvider value={unauthenticatedClient}>
           {match(route)
-            .with({ name: "PaymentLinkArea" }, () => (
-              <ScrollView>
-                <Box>
-                  <Box>
-                    <LakeLabel
-                      label={t("paymentLink.paymentMethod")}
-                      render={() => (
-                        <SegmentedControl
-                          mode="desktop"
-                          selected={selected}
-                          items={[{ name: "Direct debit", id: "sdd" }]}
-                          onValueChange={setSelected}
-                        />
-                      )}
-                    />
-                  </Box>
-
-                  <LakeLabel label={t("paymentLink.iban")} render={() => <LakeTextInput />} />
-                  <LakeLabel label={t("paymentLink.country")} render={() => <LakeTextInput />} />
-
-                  <Box direction="row">
-                    <LakeLabel
-                      label={t("paymentLink.firstName")}
-                      render={() => <LakeTextInput />}
-                    />
-
-                    <LakeLabel label={t("paymentLink.lastName")} render={() => <LakeTextInput />} />
-                  </Box>
-
-                  <LakeLabel
-                    label={t("paymentLink.addressLine1")}
-                    render={() => <LakeTextInput />}
-                  />
-
-                  <LakeLabel
-                    label={t("paymentLink.addressLine2")}
-                    render={() => <LakeTextInput />}
-                  />
-
-                  <LakeLabel label={t("paymentLink.city")} render={() => <LakeTextInput />} />
-
-                  <Box direction="row">
-                    <LakeLabel label={t("paymentLink.postcode")} render={() => <LakeTextInput />} />
-                    <LakeLabel label={t("paymentLink.state")} render={() => <LakeTextInput />} />
-                  </Box>
-
-                  <LakeButton onPress={() => {}}>
-                    <LakeText color={backgroundColor.accented}> {t("button.pay")}</LakeText>
-                  </LakeButton>
-                </Box>
-              </ScrollView>
-            ))
+            .with({ name: "PaymentLink" }, () => <PaymentPage />)
             .with(P.nullish, () => <NotFoundPage />)
             .exhaustive()}
         </ClientProvider>

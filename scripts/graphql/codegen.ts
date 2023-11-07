d76b314658ea2f00d38eddaad60ce3908ed7e088
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-11-07 17:54:16 +0100

    update package json, add form

--- scripts/graphql/codegen.ts
+++ scripts/graphql/codegen.ts
@@ -95,23 +95,6 @@ const config: CodegenConfig = {
   },
 
   generates: {
-    [file("../../clients/payment/src/graphql/unauthenticated.ts")]: {
-      documents: file("../../clients/payment/src/graphql/unauthenticated.gql"),
-      schema: file("./dist/unauthenticated-schema.gql"),
-      plugins: frontendPlugins,
-      config: frontendConfig,
-      documentTransforms: [{ transform: addTypenames }],
-    },
-
-    [file("../../clients/payment/src/graphql/introspection.json")]: {
-      schema: file("./dist/unauthenticated-schema.gql"),
-      plugins: ["introspection"],
-      config: { descriptions: false },
-      hooks: {
-        afterOneFileWrite: "yarn tsx scripts/graphql/cleanIntrospectionSchema.ts",
-      },
-    },
-
     [file("../../clients/onboarding/src/graphql/unauthenticated.ts")]: {
       documents: file("../../clients/onboarding/src/graphql/unauthenticated.gql"),
       schema: file("./dist/unauthenticated-schema.gql"),

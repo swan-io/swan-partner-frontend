1f8ddf58f77b946f52ea3da8a31a34a14ac56aba
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-11-07 17:54:16 +0100

    update package json, add form

--- types/env/index.d.ts
+++ types/env/index.d.ts
@@ -5,6 +5,7 @@ declare const __env: {
   SWAN_ENVIRONMENT: "SANDBOX" | "LIVE";
   ACCOUNT_MEMBERSHIP_INVITATION_MODE: "LINK" | "EMAIL";
   BANKING_URL: string;
+  PAYMENT_URL: string;
   IS_SWAN_MODE: boolean;
   // Client
   CLIENT_GOOGLE_MAPS_API_KEY: string;

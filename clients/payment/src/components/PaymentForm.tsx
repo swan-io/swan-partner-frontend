72e9412c1c8fb9ebd95243226a6d4e2beedd03e3
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-11-13 13:47:07 +0100

    Add gmaps autocomplete to address field

--- clients/payment/src/components/PaymentForm.tsx
+++ clients/payment/src/components/PaymentForm.tsx
@@ -9,13 +9,17 @@ import { SegmentedControl } from "@swan-io/lake/src/components/SegmentedControl"
 import { Space } from "@swan-io/lake/src/components/Space";
 import { SwanLogo } from "@swan-io/lake/src/components/SwanLogo";
 import { backgroundColor, colors, spacings } from "@swan-io/lake/src/constants/design";
+import { useBoolean } from "@swan-io/lake/src/hooks/useBoolean";
 import { useResponsive } from "@swan-io/lake/src/hooks/useResponsive";
+import { isNullish } from "@swan-io/lake/src/utils/nullish";
 import { CountryPicker } from "@swan-io/shared-business/src/components/CountryPicker";
+import { GMapAddressSearchInput } from "@swan-io/shared-business/src/components/GMapAddressSearchInput";
 import { CountryCCA3, allCountries } from "@swan-io/shared-business/src/constants/countries";
 import { useMemo } from "react";
 import { ScrollView, StyleSheet } from "react-native";
 import { hasDefinedKeys, useForm } from "react-ux-form";
 import { formatCurrency } from "../../../banking/src/utils/i18n";
+import { env } from "../utils/env";
 import { languages, locale, setPreferredLanguage, t } from "../utils/i18n";
 import { Router } from "../utils/routes";
 
@@ -71,7 +75,9 @@ type FormState = {
 export const PaymentForm = () => {
   const { desktop } = useResponsive();
 
-  const { Field, submitForm } = useForm<FormState>({
+  const [manualModeEnabled, setManualMode] = useBoolean(isNullish(env.GOOGLE_MAP_API_KEY));
+
+  const { Field, submitForm, FieldsListener, setFieldValue } = useForm<FormState>({
     paymentMethod: {
       initialValue: items[0].id,
       // validate: validateRequired,
@@ -303,41 +309,65 @@ export const PaymentForm = () => {
         </Field>
       </Box>
 
-      <Field name="addressLine1">
-        {({ value, valid, error, onChange, onBlur, ref }) => (
-          <LakeLabel
-            label={t("paymentLink.addressLine1")}
-            render={() => (
-              <LakeTextInput
-                value={value}
-                valid={valid}
-                error={error}
-                onChangeText={onChange}
-                onBlur={onBlur}
-                ref={ref}
+      <FieldsListener names={["country"]}>
+        {({ country }) => (
+          <Field name="addressLine1">
+            {({ value, onChange, error }) => (
+              <LakeLabel
+                label={t("paymentLink.addressLine1")}
+                render={id => (
+                  <GMapAddressSearchInput
+                    emptyResultText={t("common.noResults")}
+                    apiKey={__env.CLIENT_GOOGLE_MAPS_API_KEY}
+                    shouldDisplaySuggestions={!manualModeEnabled}
+                    placeholder={t("paymentLink.addressInput.placeholder")}
+                    language={locale.language}
+                    id={id}
+                    country={country.value}
+                    value={value}
+                    error={error}
+                    onValueChange={onChange}
+                    onSuggestion={suggestion => {
+                      setFieldValue("addressLine1", suggestion.completeAddress);
+                      setFieldValue("city", suggestion.city);
+                      setFieldValue("country", suggestion.country as CountryCCA3);
+                      setFieldValue("postalCode", suggestion.postalCode);
+                      setManualMode.on();
+                    }}
+                  />
+                )}
+                actions={
+                  !manualModeEnabled && desktop ? (
+                    <LakeButton mode="secondary" size="small" onPress={setManualMode.on}>
+                      {t("paymentLink.addressInput.button")}
+                    </LakeButton>
+                  ) : null
+                }
               />
             )}
-          />
+          </Field>
         )}
-      </Field>
+      </FieldsListener>
 
-      <Field name="addressLine2">
-        {({ value, valid, error, onChange, onBlur, ref }) => (
-          <LakeLabel
-            label={t("paymentLink.addressLine2")}
-            render={() => (
-              <LakeTextInput
-                value={value}
-                valid={valid}
-                error={error}
-                onChangeText={onChange}
-                onBlur={onBlur}
-                ref={ref}
-              />
-            )}
-          />
-        )}
-      </Field>
+      {manualModeEnabled && (
+        <Field name="addressLine2">
+          {({ value, valid, error, onChange, onBlur, ref }) => (
+            <LakeLabel
+              label={t("paymentLink.addressLine2")}
+              render={() => (
+                <LakeTextInput
+                  value={value}
+                  valid={valid}
+                  error={error}
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  ref={ref}
+                />
+              )}
+            />
+          )}
+        </Field>
+      )}
 
       <Field name="city">
         {({ value, valid, error, onChange, onBlur, ref }) => (

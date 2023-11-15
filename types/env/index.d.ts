6ce5cd24a6a20650f1a462f1640fba8d431f077c
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-11-15 16:23:54 +0100

    Create query, fix codegen

--- types/env/index.d.ts
+++ types/env/index.d.ts
@@ -5,9 +5,10 @@ declare const __env: {
   SWAN_ENVIRONMENT: "SANDBOX" | "LIVE";
   ACCOUNT_MEMBERSHIP_INVITATION_MODE: "LINK" | "EMAIL";
   BANKING_URL: string;
-  PAYMENT_URL: string;
   IS_SWAN_MODE: boolean;
   // Client
   CLIENT_PLACEKIT_API_KEY: string;
+  CLIENT_BANKING_URL: string;
+  CLIENT_PAYMENT_URL: string;
   CLIENT_ONBOARDING_MATOMO_SITE_ID: string;
 };

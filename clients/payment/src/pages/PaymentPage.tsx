3e93f51a67c7354e362309dcc3522b6ba4ab6f0f
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-11-17 16:29:08 +0100

    Add loading, refacto

--- clients/payment/src/pages/PaymentPage.tsx
+++ clients/payment/src/pages/PaymentPage.tsx
@@ -1,10 +1,20 @@
 import { AsyncData, Result } from "@swan-io/boxed";
+import { LoadingView } from "@swan-io/lake/src/components/LoadingView";
+import { colors } from "@swan-io/lake/src/constants/design";
 import { useUrqlQuery } from "@swan-io/lake/src/hooks/useUrqlQuery";
+import { StyleSheet } from "react-native";
 import { P, match } from "ts-pattern";
 import { ErrorView } from "../components/ErrorView";
 import { PaymentForm } from "../components/PaymentForm";
 import { GetMerchantPaymentLinkDocument } from "../graphql/unauthenticated";
 import { NotFoundPage } from "./NotFoundPage";
+
+const styles = StyleSheet.create({
+  base: {
+    backgroundColor: colors.gray[50],
+    flexGrow: 1,
+  },
+});
 
 export const PaymentPage = () => {
   const { data } = useUrqlQuery(
@@ -13,13 +23,14 @@ export const PaymentPage = () => {
     },
     [],
   );
-  console.log(data);
 
   return (
     <>
       {match(data)
         .with(AsyncData.P.NotAsked, () => null)
-        .with(AsyncData.P.Loading, () => <h1>Loading....</h1>)
+        .with(AsyncData.P.Loading, () => (
+          <LoadingView color={colors.gray[400]} style={styles.base} />
+        ))
 
         .with(
           AsyncData.P.Done(Result.P.Ok({ merchantPaymentLink: P.select() })),

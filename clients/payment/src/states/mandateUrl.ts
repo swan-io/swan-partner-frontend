644f71a2db593e67af13a8aaeb09eae26bbddac4
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-11-27 17:48:52 +0100

    Refacto, add second mutation

--- clients/payment/src/states/mandateUrl.ts
+++
@@ -1,6 +0,0 @@
-import { atom, useAtom } from "react-atomic-state";
-
-const mandateUrl = atom<string | undefined>(undefined);
-
-export const setMandateUrl = mandateUrl.set;
-export const useMandateUrl = () => useAtom(mandateUrl);

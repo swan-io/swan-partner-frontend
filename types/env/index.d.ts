700b4e5e055e35c486916d30757c7decea9d3e41
Sandrine Da Silva <sandrine.dasilva@swan.io>
2023-11-28 17:06:00 +0100

    update env var

--- types/env/index.d.ts
+++ types/env/index.d.ts
@@ -5,10 +5,11 @@ declare const __env: {
   SWAN_ENVIRONMENT: "SANDBOX" | "LIVE";
   ACCOUNT_MEMBERSHIP_INVITATION_MODE: "LINK" | "EMAIL";
   BANKING_URL: string;
+  PAYMENT_URL: string;
   IS_SWAN_MODE: boolean;
   // Client
+  CLIENT_GOOGLE_MAPS_API_KEY: string;
   CLIENT_PLACEKIT_API_KEY: string;
   CLIENT_BANKING_URL: string;
-  CLIENT_PAYMENT_URL: string;
   CLIENT_ONBOARDING_MATOMO_SITE_ID: string;
 };

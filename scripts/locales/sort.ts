7265027cb4d1597bb307c1309fe950c3a171ffd6
Mathieu Acthernoene <zoontek@gmail.com>
2023-11-28 17:05:16 +0100

    Allow yarn sort-locales

--- scripts/locales/sort.ts
+++ scripts/locales/sort.ts
@@ -1,3 +1,4 @@
+import glob from "fast-glob";
 import fs from "node:fs";
 import os from "node:os";
 
@@ -7,22 +8,21 @@ const isStringRecord = (value: unknown): value is Record<string, string> =>
   Object.values(value).every(item => typeof item === "string");
 
 // Arg automatically provided by lint-staged
-const filePath = process.argv[2];
+const filePaths =
+  process.argv[2] == null ? glob.sync("clients/**/src/locales/*.json") : [process.argv[2]];
 
-if (filePath == null) {
-  throw new Error("Missing file path");
-}
+filePaths.forEach(filePath => {
+  const content = fs.readFileSync(filePath, "utf-8");
+  const json: unknown = JSON.parse(content);
 
-const content = fs.readFileSync(filePath, "utf-8");
-const json: unknown = JSON.parse(content);
+  if (!isStringRecord(json)) {
+    throw new Error(`Invalid JSON: ${filePath}`);
+  }
 
-if (!isStringRecord(json)) {
-  throw new Error(`Invalid JSON: ${filePath}`);
-}
+  const sorted = Object.keys(json)
+    .sort()
+    .reduce<Record<string, string>>((acc, key) => ({ ...acc, [key]: json[key] as string }), {});
 
-const sorted = Object.keys(json)
-  .sort()
-  .reduce<Record<string, string>>((acc, key) => ({ ...acc, [key]: json[key] as string }), {});
-
-fs.writeFileSync(filePath, JSON.stringify(sorted, null, 2) + os.EOL, "utf-8");
-console.log(`Sorted ${filePath}`);
+  fs.writeFileSync(filePath, JSON.stringify(sorted, null, 2) + os.EOL, "utf-8");
+  console.log(`Sorted ${filePath}`);
+});

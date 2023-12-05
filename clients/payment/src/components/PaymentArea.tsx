2186070d21798f5578845cb1cd8a036b92e0caee
Mathieu Acthernoene <zoontek@gmail.com>
2023-12-05 18:16:39 +0100

    Review feedbacks

--- clients/payment/src/components/PaymentArea.tsx
+++ clients/payment/src/components/PaymentArea.tsx
@@ -1,24 +1,53 @@
 import { AsyncData, Result } from "@swan-io/boxed";
 import { LoadingView } from "@swan-io/lake/src/components/LoadingView";
-import { colors } from "@swan-io/lake/src/constants/design";
+import { backgroundColor, colors, spacings } from "@swan-io/lake/src/constants/design";
 import { useUrqlQuery } from "@swan-io/lake/src/hooks/useUrqlQuery";
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { P, match } from "ts-pattern";
 
-import { isNotNullish } from "@swan-io/lake/src/utils/nullish";
-import { useEffect, useState } from "react";
+import { AutoWidthImage } from "@swan-io/lake/src/components/AutoWidthImage";
+import { Box } from "@swan-io/lake/src/components/Box";
+import { Fill } from "@swan-io/lake/src/components/Fill";
+import { LakeButton } from "@swan-io/lake/src/components/LakeButton";
+import { LakeHeading } from "@swan-io/lake/src/components/LakeHeading";
+import { LakeSelect } from "@swan-io/lake/src/components/LakeSelect";
+import { LakeText } from "@swan-io/lake/src/components/LakeText";
+import { Space } from "@swan-io/lake/src/components/Space";
+import { SwanLogo } from "@swan-io/lake/src/components/SwanLogo";
+import { commonStyles } from "@swan-io/lake/src/constants/commonStyles";
+import { useResponsive } from "@swan-io/lake/src/hooks/useResponsive";
+import { isNotNullish, isNullish } from "@swan-io/lake/src/utils/nullish";
+import { useEffect, useMemo, useState } from "react";
 import { GetMerchantPaymentLinkDocument } from "../graphql/unauthenticated";
 import { ExpiredPage } from "../pages/ExpiredPage";
 import { NotFoundPage } from "../pages/NotFoundPage";
 import { PaymentPage } from "../pages/PaymentPage";
 import { SuccessPage } from "../pages/SuccessPage";
+import { languages, locale, setPreferredLanguage, t } from "../utils/i18n";
 import { Router } from "../utils/routes";
 import { ErrorView } from "./ErrorView";
+import { Redirect } from "./Redirect";
 
 const styles = StyleSheet.create({
   base: {
-    backgroundColor: colors.gray[50],
-    flexGrow: 1,
+    ...commonStyles.fill,
+    backgroundColor: backgroundColor.default,
+  },
+  content: {
+    marginHorizontal: "auto",
+    maxWidth: 960,
+    width: "100%",
+    paddingHorizontal: spacings[24],
+    paddingVertical: spacings[32],
+    minHeight: "100%",
+  },
+  centered: {
+    marginHorizontal: "auto",
+  },
+  swanLogo: {
+    display: "inline-flex",
+    height: 9,
+    width: 45 * (9 / 10),
   },
 });
 
@@ -27,8 +56,16 @@ type Props = {
 };
 
 export const PaymentArea = ({ paymentLinkId }: Props) => {
+  const { desktop } = useResponsive();
   const route = Router.useRoute(["PaymentForm", "PaymentSuccess"]);
   const [mandateUrl, setMandateUrl] = useState<string>();
+
+  const languageOptions = useMemo(() => {
+    return languages.map(country => ({
+      name: country.native,
+      value: country.id,
+    }));
+  }, []);
 
   const { data } = useUrqlQuery(
     {
@@ -44,26 +81,106 @@ export const PaymentArea = ({ paymentLinkId }: Props) => {
     }
   }, [mandateUrl, paymentLinkId]);
 
-  return match(data)
-    .with(AsyncData.P.NotAsked, () => null)
-    .with(AsyncData.P.Loading, () => <LoadingView color={colors.gray[400]} style={styles.base} />)
-    .with(
-      AsyncData.P.Done(
-        Result.P.Ok({ merchantPaymentLink: P.select({ statusInfo: { status: "Active" } }) }),
-      ),
-      merchantPaymentLink =>
-        match(route)
-          .with({ name: "PaymentForm" }, () => (
-            <PaymentPage paymentLink={merchantPaymentLink} setMandateUrl={setMandateUrl} />
-          ))
-          .with({ name: "PaymentSuccess" }, () => (
-            <SuccessPage paymentLink={merchantPaymentLink} mandateUrl={mandateUrl} />
-          ))
-          .otherwise(() => <NotFoundPage />),
-    )
-    .with(
-      AsyncData.P.Done(Result.P.Ok({ merchantPaymentLink: P.select(P.not(P.nullish)) })),
-      merchantPaymentLink => <ExpiredPage paymentLink={merchantPaymentLink} />,
-    )
-    .otherwise(() => <ErrorView />);
+  return (
+    <View style={styles.base}>
+      {match(data)
+        .with(AsyncData.P.NotAsked, () => null)
+        .with(AsyncData.P.Loading, () => <LoadingView color={colors.gray[400]} />)
+        .with(
+          AsyncData.P.Done(Result.P.Ok({ merchantPaymentLink: P.select(P.not(P.nullish)) })),
+          merchantPaymentLink => {
+            const { cancelRedirectUrl, merchantProfile, statusInfo } = merchantPaymentLink;
+            const { merchantLogoUrl, merchantName } = merchantProfile;
+            const status = isNullish(mandateUrl) ? statusInfo.status : "Completed";
+
+            return (
+              <ScrollView contentContainerStyle={styles.content}>
+                {route?.name === "PaymentForm" && (
+                  <>
+                    <Box direction="row" alignItems="center">
+                      <LakeButton
+                        ariaLabel={t("common.back")}
+                        icon="arrow-left-filled"
+                        mode="tertiary"
+                        onPress={() => {
+                          window.location.replace(cancelRedirectUrl);
+                        }}
+                      >
+                        {desktop ? t("common.back") : null}
+                      </LakeButton>
+
+                      <Fill minWidth={16} />
+
+                      <View>
+                        <LakeSelect
+                          value={locale.language}
+                          items={languageOptions}
+                          hideErrors={true}
+                          mode="borderless"
+                          onValueChange={locale => {
+                            setPreferredLanguage(locale);
+                          }}
+                        />
+                      </View>
+                    </Box>
+
+                    <Space height={24} />
+                  </>
+                )}
+
+                {isNotNullish(merchantLogoUrl) ? (
+                  <AutoWidthImage
+                    alt={merchantName}
+                    height={40}
+                    maxWidth={180}
+                    resizeMode="contain"
+                    sourceUri={merchantLogoUrl}
+                    style={styles.centered}
+                  />
+                ) : (
+                  <LakeHeading variant="h3" level={3} align="center">
+                    {merchantName}
+                  </LakeHeading>
+                )}
+
+                <Space height={24} />
+
+                {match({ route: route?.name, status })
+                  .with({ route: "PaymentForm", status: "Active" }, () => (
+                    <PaymentPage paymentLink={merchantPaymentLink} setMandateUrl={setMandateUrl} />
+                  ))
+                  .with({ route: "PaymentSuccess", status: "Completed" }, () => (
+                    <SuccessPage paymentLink={merchantPaymentLink} mandateUrl={mandateUrl} />
+                  ))
+                  .with({ route: "PaymentSuccess" }, () => (
+                    <Redirect to={Router.PaymentForm({ paymentLinkId })} />
+                  ))
+                  .with({ status: "Expired" }, () => (
+                    <ExpiredPage paymentLink={merchantPaymentLink} />
+                  ))
+                  .otherwise(() => (
+                    <NotFoundPage />
+                  ))}
+
+                <Space height={32} />
+
+                <Box
+                  role="contentinfo"
+                  direction="row"
+                  alignItems="baseline"
+                  style={route?.name !== "PaymentForm" && styles.centered}
+                >
+                  <LakeText variant="smallRegular">{t("paymentLink.poweredBySwan")}</LakeText>
+                  <Space width={4} />
+                  <SwanLogo color={colors.swan[500]} style={styles.swanLogo} />
+                </Box>
+              </ScrollView>
+            );
+          },
+        )
+        .otherwise(() => (
+          <ErrorView />
+        ))}
+    </View>
+  );
 };
--- clients/payment/src/pages/SuccessPage.tsx
+++ clients/payment/src/pages/SuccessPage.tsx
@@ -1,34 +1,23 @@
 import { BorderedIcon } from "@swan-io/lake/src/components/BorderedIcon";
 import { Box } from "@swan-io/lake/src/components/Box";
 import { LakeButton } from "@swan-io/lake/src/components/LakeButton";
-import { LakeHeading } from "@swan-io/lake/src/components/LakeHeading";
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
 import { Space } from "@swan-io/lake/src/components/Space";
-import { SwanLogo } from "@swan-io/lake/src/components/SwanLogo";
+import { commonStyles } from "@swan-io/lake/src/constants/commonStyles";
 import { colors, spacings } from "@swan-io/lake/src/constants/design";
 import { useResponsive } from "@swan-io/lake/src/hooks/useResponsive";
 import { isNotNullish } from "@swan-io/lake/src/utils/nullish";
 import { StyleSheet } from "react-native";
 import { GetMerchantPaymentLinkQuery } from "../graphql/unauthenticated";
 import { t } from "../utils/i18n";
-import { IMAGE_STYLE } from "./PaymentPage";
 
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    paddingVertical: spacings[40],
+  fill: {
+    ...commonStyles.fill,
   },
-  containerItems: {
+  mobileButtons: {
+    paddingHorizontal: spacings[16],
     width: "100%",
-  },
-  containerButtonsDesktop: {
-    width: "auto",
-  },
-  containerButtons: { width: "100%", paddingHorizontal: spacings[24] },
-  swanLogo: {
-    display: "inline-flex",
-    height: 9,
-    width: 45 * (9 / 10),
   },
   title: {
     paddingBottom: spacings[4],
@@ -47,68 +36,44 @@ export const SuccessPage = ({ paymentLink, mandateUrl }: Props) => {
   const { desktop } = useResponsive();
 
   return (
-    <Box
-      direction="column"
-      alignItems="center"
-      justifyContent="spaceBetween"
-      style={styles.container}
-    >
-      {isNotNullish(paymentLink.merchantProfile.merchantLogoUrl) ? (
-        <img src={paymentLink.merchantProfile.merchantLogoUrl} style={IMAGE_STYLE} />
-      ) : (
-        <LakeHeading variant="h3" level={3} align="center">
-          {paymentLink.merchantProfile.merchantName}
-        </LakeHeading>
-      )}
+    <Box alignItems="center" justifyContent="center" style={styles.fill}>
+      <BorderedIcon name={"lake-check"} color="positive" size={70} padding={16} />
+      <Space height={24} />
 
-      <Box direction="column" alignItems="center" style={styles.containerItems}>
-        <BorderedIcon name={"lake-check"} color="positive" size={70} padding={16} />
-        <Space height={24} />
+      <LakeText variant="semibold" color={colors.gray[900]} style={styles.title}>
+        {t("paymentLink.success.title")}
+      </LakeText>
 
-        <LakeText variant="semibold" color={colors.gray[900]} style={styles.title}>
-          {t("paymentLink.success.title")}
-        </LakeText>
+      <LakeText align="center" color={colors.gray[500]} style={styles.subtitle}>
+        {t("paymentLink.success.subtitle")}
+      </LakeText>
 
-        <LakeText align="center" color={colors.gray[500]} style={styles.subtitle}>
-          {t("paymentLink.success.subtitle")}
-        </LakeText>
+      <Space height={32} />
 
-        <Space height={32} />
-
-        <Box
-          direction={desktop ? "row" : "column"}
-          style={desktop ? styles.containerButtonsDesktop : styles.containerButtons}
+      <Box direction={desktop ? "row" : "column"} style={!desktop && styles.mobileButtons}>
+        <LakeButton
+          ariaLabel={t("paymentLink.button.returnToWebsite")}
+          mode="secondary"
+          href={paymentLink.redirectUrl}
         >
-          <LakeButton
-            ariaLabel={t("paymentLink.button.returnToWebsite")}
-            mode="secondary"
-            grow={true}
-            href={paymentLink.redirectUrl}
-          >
-            {t("paymentLink.button.returnToWebsite")}
-          </LakeButton>
+          {t("paymentLink.button.returnToWebsite")}
+        </LakeButton>
 
-          <Space height={12} width={16} />
+        {isNotNullish(mandateUrl) && (
+          <>
+            <Space height={12} width={16} />
 
-          {isNotNullish(mandateUrl) && (
             <LakeButton
               color="current"
               ariaLabel={t("paymentLink.button.downloadMandate")}
               icon="arrow-down-regular"
               mode="primary"
-              grow={true}
               href={mandateUrl}
             >
               {t("paymentLink.button.downloadMandate")}
             </LakeButton>
-          )}
-        </Box>
-      </Box>
-
-      <Box direction="row" alignItems="baseline">
-        <LakeText>{t("paymentLink.poweredBySwan")}</LakeText>
-        <Space width={4} />
-        <SwanLogo color={colors.swan[500]} style={styles.swanLogo} />
+          </>
+        )}
       </Box>
     </Box>
   );

24ce25844c7a18baecf84fb00e57d565d606a535
Mathieu Acthernoene <zoontek@gmail.com>
2023-12-05 11:50:11 +0100

    Update dependencies (#477)

--- server/src/utils/fetch.ts
+++ server/src/utils/fetch.ts
@@ -2,7 +2,7 @@ export const fetchWithTimeout: typeof fetch = async (input, init) => {
   const controller = new AbortController();
   const id = setTimeout(() => controller.abort(), 30_000);
 
-  const response = await fetch(input as NodeJS.fetch.RequestInfo, {
+  const response = await fetch(input, {
     ...init,
     signal: controller.signal,
   });

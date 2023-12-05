eeb6b1c98f1471cc762c8e79cce1527ebc3939cf
Mathieu Acthernoene <zoontek@gmail.com>
2023-12-05 18:16:39 +0100

    Review feedbacks

--- clients/payment/src/pages/ErrorPage.tsx
+++ clients/payment/src/pages/ErrorPage.tsx
@@ -1,28 +1,15 @@
 import { BorderedIcon } from "@swan-io/lake/src/components/BorderedIcon";
 import { Box } from "@swan-io/lake/src/components/Box";
-import { LakeHeading } from "@swan-io/lake/src/components/LakeHeading";
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
 import { Space } from "@swan-io/lake/src/components/Space";
-import { SwanLogo } from "@swan-io/lake/src/components/SwanLogo";
+import { commonStyles } from "@swan-io/lake/src/constants/commonStyles";
 import { colors, spacings } from "@swan-io/lake/src/constants/design";
-import { isNotNullish } from "@swan-io/lake/src/utils/nullish";
 import { StyleSheet } from "react-native";
-import { GetMerchantPaymentLinkQuery } from "../graphql/unauthenticated";
 import { t } from "../utils/i18n";
-import { IMAGE_STYLE } from "./PaymentPage";
 
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    paddingVertical: spacings[40],
-  },
-  containerItems: {
-    width: "100%",
-  },
-  swanLogo: {
-    display: "inline-flex",
-    height: 9,
-    width: 45 * (9 / 10),
+  fill: {
+    ...commonStyles.fill,
   },
   title: {
     paddingBottom: spacings[4],
@@ -32,44 +19,19 @@ const styles = StyleSheet.create({
   },
 });
 
-type Props = {
-  paymentLink: NonNullable<GetMerchantPaymentLinkQuery["merchantPaymentLink"]>;
-};
-
-export const ErrorPage = ({ paymentLink }: Props) => {
+export const ErrorPage = () => {
   return (
-    <Box
-      direction="column"
-      alignItems="center"
-      justifyContent="spaceBetween"
-      style={styles.container}
-    >
-      {isNotNullish(paymentLink.merchantProfile.merchantLogoUrl) ? (
-        <img src={paymentLink.merchantProfile.merchantLogoUrl} style={IMAGE_STYLE} />
-      ) : (
-        <LakeHeading variant="h3" level={3} align="center">
-          {paymentLink.merchantProfile.merchantName}
-        </LakeHeading>
-      )}
+    <Box alignItems="center" justifyContent="center" style={styles.fill}>
+      <BorderedIcon name={"lake-warning"} color="negative" size={70} padding={16} />
+      <Space height={24} />
 
-      <Box direction="column" alignItems="center" style={styles.containerItems}>
-        <BorderedIcon name={"lake-warning"} color="negative" size={70} padding={16} />
-        <Space height={24} />
+      <LakeText variant="semibold" color={colors.gray[900]} style={styles.title}>
+        {t("paymentLink.error.title")}
+      </LakeText>
 
-        <LakeText variant="semibold" color={colors.gray[900]} style={styles.title}>
-          {t("paymentLink.error.title")}
-        </LakeText>
-
-        <LakeText align="center" color={colors.gray[500]} style={styles.subtitle}>
-          {t("paymentLink.error.subtitle")}
-        </LakeText>
-      </Box>
-
-      <Box direction="row" alignItems="baseline">
-        <LakeText>{t("paymentLink.poweredBySwan")}</LakeText>
-        <Space width={4} />
-        <SwanLogo color={colors.swan[500]} style={styles.swanLogo} />
-      </Box>
+      <LakeText align="center" color={colors.gray[500]} style={styles.subtitle}>
+        {t("paymentLink.error.subtitle")}
+      </LakeText>
     </Box>
   );
 };
--- clients/payment/src/pages/PaymentPage.tsx
+++ clients/payment/src/pages/PaymentPage.tsx
@@ -3,24 +3,20 @@ import { Box } from "@swan-io/lake/src/components/Box";
 import { LakeButton } from "@swan-io/lake/src/components/LakeButton";
 import { LakeHeading } from "@swan-io/lake/src/components/LakeHeading";
 import { LakeLabel } from "@swan-io/lake/src/components/LakeLabel";
-import { LakeSelect } from "@swan-io/lake/src/components/LakeSelect";
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
 import { LakeTextInput } from "@swan-io/lake/src/components/LakeTextInput";
 import { SegmentedControl } from "@swan-io/lake/src/components/SegmentedControl";
 import { Space } from "@swan-io/lake/src/components/Space";
-import { SwanLogo } from "@swan-io/lake/src/components/SwanLogo";
-import { backgroundColor, colors, spacings } from "@swan-io/lake/src/constants/design";
+import { colors } from "@swan-io/lake/src/constants/design";
 import { useResponsive } from "@swan-io/lake/src/hooks/useResponsive";
 import { useUrqlMutation } from "@swan-io/lake/src/hooks/useUrqlMutation";
 import { showToast } from "@swan-io/lake/src/state/toasts";
-import { isNotNullish } from "@swan-io/lake/src/utils/nullish";
 import { filterRejectionsToResult } from "@swan-io/lake/src/utils/urql";
 import { CountryPicker } from "@swan-io/shared-business/src/components/CountryPicker";
 import { CountryCCA3, allCountries } from "@swan-io/shared-business/src/constants/countries";
 import { translateError } from "@swan-io/shared-business/src/utils/i18n";
 import { validateRequired } from "@swan-io/shared-business/src/utils/validation";
-import { CSSProperties, useMemo } from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { combineValidators, hasDefinedKeys, useForm } from "react-ux-form";
 import { P, match } from "ts-pattern";
 import { formatCurrency } from "../../../banking/src/utils/i18n";
@@ -31,41 +27,19 @@ import {
   GetMerchantPaymentLinkQuery,
   InitiateSddPaymentCollectionDocument,
 } from "../graphql/unauthenticated";
-import { languages, locale, setPreferredLanguage, t } from "../utils/i18n";
-
-export const IMAGE_STYLE: CSSProperties = {
-  top: 0,
-  left: 0,
-  alignSelf: "center",
-  maxWidth: "200px",
-};
+import { t } from "../utils/i18n";
 
 const styles = StyleSheet.create({
-  container: {
-    marginHorizontal: "auto",
-    maxWidth: 960,
-    width: "100%",
-    paddingHorizontal: spacings[24],
-    paddingVertical: spacings[32],
-  },
-  label: {
-    flexBasis: "50%",
-    flexShrink: 1,
-    alignSelf: "stretch",
-  },
   segmentedControlDesktop: {
     maxWidth: "50%",
   },
   segmentedControl: {
     maxWidth: "100%",
   },
-  swanLogo: {
-    display: "inline-flex",
-    height: 9,
-    width: 45 * (9 / 10),
-  },
-  selectLanguage: {
-    alignItems: "flex-end",
+  half: {
+    flexBasis: "0%",
+    flexGrow: 1,
+    flexShrink: 1,
   },
 });
 
@@ -125,8 +99,6 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
         const country = getFieldState("country").value;
         if (sepaNonEeaCountries.includes(country)) {
           return validateRequired(value);
-        } else {
-          return;
         }
       },
       sanitize: value => value.trim(),
@@ -138,8 +110,6 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
         const country = getFieldState("country").value;
         if (sepaNonEeaCountries.includes(country)) {
           return validateRequired(value);
-        } else {
-          return;
         }
       },
     },
@@ -150,8 +120,6 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
         const country = getFieldState("country").value;
         if (sepaNonEeaCountries.includes(country)) {
           return validateRequired(value);
-        } else {
-          return;
         }
       },
     },
@@ -252,64 +220,12 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
     });
   };
 
-  const languageOptions = useMemo(() => {
-    return languages.map(country => ({
-      name: country.native,
-      value: country.id,
-    }));
-  }, []);
-
   const merchantName = paymentLink.merchantProfile.merchantName;
 
   return (
-    <ScrollView
-      style={{
-        backgroundColor: backgroundColor.default,
-      }}
-      contentContainerStyle={styles.container}
-    >
-      <Box direction="row" alignItems="center" justifyContent="spaceBetween">
-        <Box>
-          <LakeButton
-            ariaLabel={t("common.back")}
-            icon="arrow-left-filled"
-            mode="tertiary"
-            grow={true}
-            onPress={() => {
-              window.location.replace(paymentLink.cancelRedirectUrl);
-            }}
-          >
-            {desktop ? t("common.back") : null}
-          </LakeButton>
-        </Box>
-
-        <Box style={styles.selectLanguage}>
-          <LakeSelect
-            value={locale.language}
-            items={languageOptions}
-            hideErrors={true}
-            mode="borderless"
-            onValueChange={locale => {
-              setPreferredLanguage(locale);
-            }}
-          />
-        </Box>
-      </Box>
-
-      <Space height={24} />
-
-      {isNotNullish(paymentLink.merchantProfile.merchantLogoUrl) ? (
-        <img src={paymentLink.merchantProfile.merchantLogoUrl} style={IMAGE_STYLE} />
-      ) : (
-        <LakeHeading variant="h3" level={3} align="center">
-          {paymentLink.merchantProfile.merchantName}
-        </LakeHeading>
-      )}
-
-      <Space height={24} />
-
+    <>
       <LakeText variant="medium" align="center" color={colors.gray[700]}>
-        {paymentLink?.label}
+        {paymentLink.label}
       </LakeText>
 
       <Space height={12} />
@@ -395,7 +311,6 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
                 ref={ref}
               />
             )}
-            style={styles.label}
           />
         )}
       </Field>
@@ -441,6 +356,7 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
           {({ value, valid, error, onChange, onBlur, ref }) => (
             <LakeLabel
               label={t("paymentLink.postalCode")}
+              style={desktop && styles.half}
               render={() => (
                 <LakeTextInput
                   value={value}
@@ -451,7 +367,6 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
                   ref={ref}
                 />
               )}
-              style={styles.label}
             />
           )}
         </Field>
@@ -461,8 +376,8 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
         <Field name="state">
           {({ value, valid, error, onChange, onBlur, ref }) => (
             <LakeLabel
-              style={styles.label}
               label={t("paymentLink.state")}
+              style={desktop && styles.half}
               render={() => (
                 <LakeTextInput
                   value={value}
@@ -488,7 +403,7 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
           initiateSddPaymentCollectionData.isLoading()
         }
       >
-        <LakeText color={colors.gray[50]}> {t("button.pay")}</LakeText>
+        {t("button.pay")}
       </LakeButton>
 
       <Space height={32} />
@@ -496,14 +411,6 @@ export const PaymentPage = ({ paymentLink, setMandateUrl }: Props) => {
       <LakeText color={colors.gray[700]} align="center" variant="smallRegular">
         {t("paymentLink.termsAndConditions", { merchantName })}
       </LakeText>
-
-      <Space height={32} />
-
-      <Box direction="row" alignItems="baseline">
-        <LakeText>{t("paymentLink.poweredBySwan")}</LakeText>
-        <Space width={4} />
-        <SwanLogo color={colors.swan[500]} style={styles.swanLogo} />
-      </Box>
-    </ScrollView>
+    </>
   );
 };

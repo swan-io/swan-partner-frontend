b5768b7e003bfaad851d3fa06d858ea3bfecd723
Mathieu Acthernoene <zoontek@gmail.com>
2024-01-05 10:53:29 +0100

    Update NotFoundPage

--- clients/onboarding/src/pages/NotFoundPage.tsx
+++ clients/onboarding/src/pages/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import { BorderedIcon } from "@swan-io/lake/src/components/BorderedIcon";
 import { Box } from "@swan-io/lake/src/components/Box";
 import { LakeHeading } from "@swan-io/lake/src/components/LakeHeading";
+import { LakeText } from "@swan-io/lake/src/components/LakeText";
 import { Space } from "@swan-io/lake/src/components/Space";
 import { StyleProp, StyleSheet, ViewStyle } from "react-native";
 import { t } from "../utils/i18n";
@@ -13,16 +14,25 @@ const styles = StyleSheet.create({
 });
 
 type Props = {
+  title?: string;
+  text?: string;
   style?: StyleProp<ViewStyle>;
 };
 
-export const NotFoundPage = ({ style }: Props) => (
+export const NotFoundPage = ({ title = t("error.pageNotFound"), text = "", style }: Props) => (
   <Box alignItems="center" justifyContent="center" style={[styles.base, style]}>
-    <BorderedIcon color="partner" name="lake-compass" padding={24} size={100} />
+    <BorderedIcon color="swan" name="lake-compass" padding={24} size={100} />
     <Space height={24} />
 
     <LakeHeading level={1} variant="h3" align="center">
-      {t("error.pageNotFound")}
+      {title}
     </LakeHeading>
+
+    {text !== "" && (
+      <>
+        <Space height={4} />
+        <LakeText variant="smallRegular">{text}</LakeText>
+      </>
+    )}
   </Box>
 );

f0a98d5e126781566efa4af76848150f59796ff6
Sandrine Da Silva <sandrine.dasilva@swan.io>
2024-01-29 13:57:03 +0100

    add placeholder, change fileds layout

--- clients/payment/src/components/Card.tsx
+++ clients/payment/src/components/Card.tsx
@@ -1,10 +1,21 @@
+import { Box } from "@swan-io/lake/src/components/Box";
 import { LakeLabel } from "@swan-io/lake/src/components/LakeLabel";
+import { Space } from "@swan-io/lake/src/components/Space";
+import { useResponsive } from "@swan-io/lake/src/hooks/useResponsive";
 import { Frames } from "frames-react";
 import { useEffect } from "react";
+import { StyleSheet } from "react-native";
 import { env } from "../utils/env";
-import { t } from "../utils/i18n";
+
+const styles = StyleSheet.create({
+  grow: {
+    flexGrow: 1,
+  },
+});
 
 export const Card = () => {
+  const { desktop } = useResponsive();
+
   useEffect(() => {
     Frames.init({
       publicKey: env.CLIENT_CHECKOUT_API_KEY,
@@ -25,35 +36,52 @@ export const Card = () => {
           backgroundColor: "#ffffff",
         },
       },
+      localization: {
+        cardNumberPlaceholder: "1234 12•• •••• 1234",
+        cvvPlaceholder: "XXX",
+        expiryMonthPlaceholder: "MM",
+        expiryYearPlaceholder: "YY",
+      },
     });
   }, []);
 
   return (
     <>
+      {/* We need this style tag to remove the default style provided by Checkout */}
       <style>
         {".card-number-frame, .expiry-date-frame, .cvv-frame, iframe { height: 62px !important; }"}
       </style>
 
       <LakeLabel
-        label={t("paymentLink.postalCode")}
+        label={"card number"}
         render={() => (
           <div className="card-number-frame">{/* <!-- card number will be added here --> */}</div>
         )}
       />
 
-      <LakeLabel
-        label={t("paymentLink.postalCode")}
-        render={() => (
-          <div className="expiry-date-frame">{/* <!-- expiry date will be added here --> */}</div>
-        )}
-      />
+      <Box direction={desktop ? "row" : "column"}>
+        <Box style={styles.grow}>
+          <LakeLabel
+            label={"expiry date"}
+            render={() => (
+              <div className="expiry-date-frame">
+                {/* <!-- expiry date will be added here --> */}
+              </div>
+            )}
+          />
+        </Box>
 
-      <LakeLabel
-        label={t("paymentLink.postalCode")}
-        render={() => (
-          <div className="cvv-frame">{/* <!-- cvv frame will be added here --> */}</div>
-        )}
-      />
+        <Space width={24} />
+
+        <Box style={styles.grow}>
+          <LakeLabel
+            label={"CVV"}
+            render={() => (
+              <div className="cvv-frame">{/* <!-- cvv frame will be added here --> */}</div>
+            )}
+          />
+        </Box>
+      </Box>
 
       <button
         onClick={() => {

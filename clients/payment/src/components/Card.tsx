a2984afc8cb4edaa70a8ec1627b8470e934774eb
Sandrine Da Silva <sandrine.dasilva@swan.io>
2024-01-29 10:43:46 +0100

    wip

--- clients/payment/src/components/Card.tsx
+++ clients/payment/src/components/Card.tsx
@@ -1,17 +1,48 @@
+import { LakeLabel } from "@swan-io/lake/src/components/LakeLabel";
 import { Frames } from "frames-react";
 import { useEffect } from "react";
 import { env } from "../utils/env";
+import { t } from "../utils/i18n";
 
 export const Card = () => {
   useEffect(() => {
     Frames.init({
       publicKey: env.CLIENT_CHECKOUT_API_KEY,
+      style: {
+        base: {
+          borderStyle: "solid",
+          borderWidth: "1px",
+          borderColor: "#e8e7e8",
+          borderRadius: "6px",
+          height: "40px",
+          paddingLeft: "12px",
+          fontFamily:
+            "Inter, -apple-system, system-ui, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+          fontSize: "16px",
+          // fontStyle: "normal",
+          color: "#2b2f30",
+          letterSpacing: "-0.011em",
+          backgroundColor: "#ffffff",
+        },
+      },
     });
   }, []);
 
   return (
     <>
-      <div className="card-number-frame">{/* <!-- card number will be added here --> */}</div>
+      <LakeLabel
+        label={t("paymentLink.postalCode")}
+        render={() => (
+          <div className="card-number-frame">{/* <!-- card number will be added here --> */}</div>
+        )}
+      />
+
+      <LakeLabel
+        label={t("paymentLink.postalCode")}
+        render={() => (
+          <div className="expiry-date-frame">{/* <!-- expiry date will be added here --> */}</div>
+        )}
+      />
       <div className="expiry-date-frame">{/* <!-- expiry date will be added here --> */}</div>
       <div className="cvv-frame">{/* <!-- cvv frame will be added here --> */}</div>
 

3f4e92a06d1c5d1449b402dd6a08a5bc1404ecdc
Matthias Le Brun <bloodyowl@icloud.com>
2024-02-06 14:35:53 +0100

    Add missing translation on individual onboarding (#594)
    Closes FRONT-870

--- clients/onboarding/src/pages/individual/OnboardingIndividualEmail.tsx
+++ clients/onboarding/src/pages/individual/OnboardingIndividualEmail.tsx
@@ -154,7 +154,7 @@ export const OnboardingIndividualEmail = ({
             <>
               <StepTitle isMobile={small}>{t("individual.step.email.title")}</StepTitle>
               <Space height={small ? 8 : 12} />
-              <LakeText>{t("individual.step.introduction.description")}</LakeText>
+              <LakeText>{t("individual.step.email.description")}</LakeText>
               <Space height={small ? 24 : 32} />
 
               <Tile>

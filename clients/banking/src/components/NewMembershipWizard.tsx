6ecbb5725483104938fefb9a229094f757215f9d
Nicolas Comont <contact@nicolasc.eu>
2024-04-09 11:25:22 +0200

    fix(front-938): number are not allowed in web banking membership invitation form

--- clients/banking/src/components/NewMembershipWizard.tsx
+++ clients/banking/src/components/NewMembershipWizard.tsx
@@ -34,9 +34,9 @@ import { projectConfiguration } from "../utils/projectId";
 import { Router } from "../utils/routes";
 import {
   validateAddressLine,
-  validateBeneficiaryName,
   validateBirthdate,
   validateEmail,
+  validateName,
   validateRequired,
 } from "../utils/validations";
 
@@ -164,13 +164,13 @@ export const NewMembershipWizard = ({
     firstName: {
       initialValue: partiallySavedValues?.firstName ?? "",
       strategy: "onBlur",
-      validate: validateBeneficiaryName,
+      validate: validateName,
       sanitize: value => value.trim(),
     },
     lastName: {
       initialValue: partiallySavedValues?.lastName ?? "",
       strategy: "onBlur",
-      validate: validateBeneficiaryName,
+      validate: validateName,
       sanitize: value => value.trim(),
     },
     birthDate: {

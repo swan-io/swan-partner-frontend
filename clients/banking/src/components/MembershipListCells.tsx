8e2817f98860b5517752fe4e51edbc33a7f453a3
Nicolas Comont <contact@nicolasc.eu>
2024-04-09 17:44:19 +0200

    chore: bump to lake 7.4.3

--- clients/banking/src/components/MembershipListCells.tsx
+++ clients/banking/src/components/MembershipListCells.tsx
@@ -47,7 +47,6 @@ const styles = StyleSheet.create({
     width: 1,
     alignSelf: "stretch",
     backgroundColor: colors.gray[200],
-    height: 28,
     marginHorizontal: spacings[4],
   },
   notificationPill: {

89bcfe0946e5e94dee74ef808fc946dc2d286ea2
Mathieu Acthernoene <zoontek@gmail.com>
2024-05-22 11:43:11 +0200

    Add "".at(), [].at() and "".replaceAll() polyfills (#720)

--- clients/onboarding/src/index.tsx
+++ clients/onboarding/src/index.tsx
@@ -6,6 +6,8 @@ import { ResizeObserver } from "@juggle/resize-observer";
 import "core-js/proposals/array-flat-map";
 import "core-js/proposals/change-array-by-copy-stage-4";
 import "core-js/proposals/object-from-entries";
+import "core-js/proposals/relative-indexing-method";
+import "core-js/proposals/string-replace-all-stage-4";
 
 import { isNullish } from "@swan-io/lake/src/utils/nullish";
 import { AppRegistry } from "react-native";

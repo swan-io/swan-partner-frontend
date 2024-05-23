9484c9cebed08538390dd74a325911c000f14d98
Mathieu Acthernoene <zoontek@gmail.com>
2024-05-23 14:59:48 +0200

    Revoke Sentry token

--- scripts/sentry/sentryRelease.mjs
+++ scripts/sentry/sentryRelease.mjs
@@ -1,17 +1,17 @@
 import SentryCli from "@sentry/cli";
 import fs from "fs";
+import assert from "node:assert";
 import path from "pathe";
 import url from "url";
 
 const dirname = path.dirname(url.fileURLToPath(import.meta.url));
-
 const { version } = JSON.parse(fs.readFileSync(path.join(dirname, "../../package.json"), "utf-8"));
+
+assert(process.env.SENTRY_AUTH_TOKEN);
 
 process.env["SENTRY_PIPELINE"] = `gitlab/${version}`;
 // The following aren't exposed in the CI
 process.env["SENTRY_ORG"] = "swan-bank";
-process.env["SENTRY_AUTH_TOKEN"] =
-  "4a366b1900224e84b87c093aa36c81312ec96eec3a9f4ee39d912b58d82e71b2";
 
 async function release() {
   const cli = new SentryCli().releases;

7dac2ce0ec4b09dca615f1d48f425af70a8b22f0
Mathieu Acthernoene <zoontek@gmail.com>
2024-06-21 12:11:36 +0200

    Make sure shared-business supported languages are overwritten

--- clients/payment/src/index.tsx
+++ clients/payment/src/index.tsx
@@ -3,7 +3,14 @@ import "@swan-io/lake/src/assets/main.css";
 import "./main.css";
 
 import { ResizeObserver } from "@juggle/resize-observer";
-import "core-js/actual/array/flat";
+import "core-js/proposals/array-flat-map";
+import "core-js/proposals/change-array-by-copy-stage-4";
+import "core-js/proposals/object-from-entries";
+import "core-js/proposals/relative-indexing-method";
+import "core-js/proposals/string-replace-all-stage-4";
+
+// overrides shared-business supported languages
+import "./utils/i18n";
 
 import { isNullish } from "@swan-io/lake/src/utils/nullish";
 import { AppRegistry } from "react-native";

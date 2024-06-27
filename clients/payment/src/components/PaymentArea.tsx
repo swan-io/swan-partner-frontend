2677d2bb1564b6c34c588923f62bdfd3db463023
Sandrine Da Silva <sandrine.dasilva@swan.io>
2024-06-27 10:29:26 +0200

    If payment completed should be redirected to success page

--- clients/payment/src/components/PaymentArea.tsx
+++ clients/payment/src/components/PaymentArea.tsx
@@ -139,7 +139,12 @@ export const PaymentArea = ({ paymentLinkId }: Props) => {
 
                 <Space height={24} />
 
-                {match({ route: route?.name, params: route?.params, mandateUrlStatus })
+                {match({
+                  route: route?.name,
+                  params: route?.params,
+                  mandateUrlStatus,
+                  merchantPaymentLink,
+                })
                   .with({ route: "PaymentForm", mandateUrlStatus: "Active" }, () =>
                     match({
                       paymentMethods: merchantPaymentLink.paymentMethods,
@@ -158,6 +163,9 @@ export const PaymentArea = ({ paymentLinkId }: Props) => {
                       )
                       .otherwise(() => <ErrorView />),
                   )
+                  .with({ merchantPaymentLink: { statusInfo: { status: "Completed" } } }, () => (
+                    <SuccessPage mandateUrl={mandateUrl} />
+                  ))
                   .with({ params: { error: "true" } }, () => <CardErrorPage />)
                   .with({ route: "PaymentSuccess", mandateUrlStatus: "Completed" }, () => (
                     <SuccessPage mandateUrl={mandateUrl} />

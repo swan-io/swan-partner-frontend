c94c17cedac73b62157ea535821ba29e66451ba6
Mathieu Acthernoene <zoontek@gmail.com>
2024-06-28 18:27:18 +0200

    WIP

--- clients/banking/src/components/BeneficiaryList.tsx
+++ clients/banking/src/components/BeneficiaryList.tsx
@@ -1,44 +1,183 @@
 import { AsyncData, Option, Result } from "@swan-io/boxed";
 import { useForwardPagination, useQuery } from "@swan-io/graphql-client";
-import { Box } from "@swan-io/lake/src/components/Box";
+import { Box, BoxProps } from "@swan-io/lake/src/components/Box";
 import {
   FixedListViewEmpty,
   PlainListViewPlaceholder,
 } from "@swan-io/lake/src/components/FixedListView";
+import { SimpleHeaderCell } from "@swan-io/lake/src/components/FixedListViewCells";
+import { IconName } from "@swan-io/lake/src/components/Icon";
 import { LakeButton } from "@swan-io/lake/src/components/LakeButton";
-import { PlainListView } from "@swan-io/lake/src/components/PlainListView";
+import { LakeText } from "@swan-io/lake/src/components/LakeText";
+import { ColumnConfig, PlainListView } from "@swan-io/lake/src/components/PlainListView";
 import { ResponsiveContainer } from "@swan-io/lake/src/components/ResponsiveContainer";
 import { Space } from "@swan-io/lake/src/components/Space";
+import { Tag } from "@swan-io/lake/src/components/Tag";
 import { commonStyles } from "@swan-io/lake/src/constants/commonStyles";
-import { breakpoints, spacings } from "@swan-io/lake/src/constants/design";
+import { breakpoints, colors, spacings } from "@swan-io/lake/src/constants/design";
+import { GetNode } from "@swan-io/lake/src/utils/types";
+import { printFormat } from "iban";
+import { useMemo } from "react";
 import { StyleSheet } from "react-native";
 import { P, match } from "ts-pattern";
-import { BeneficiariesListPageDocument, BeneficiariesListPageQuery } from "../graphql/partner";
+import {
+  BeneficiariesListPageDocument,
+  BeneficiariesListPageQuery,
+  BeneficiariesListPageQueryVariables,
+} from "../graphql/partner";
 import { Router } from "../utils/routes";
 import { ErrorView } from "./ErrorView";
 
 const NUM_TO_RENDER = 20;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cell: {
+    paddingHorizontal: spacings[16],
+  },
+});
 
 type Account = NonNullable<BeneficiariesListPageQuery["account"]>;
 type Beneficiaries = NonNullable<Account["trustedBeneficiaries"]>;
 
+const Cell = (props: BoxProps) => (
+  <Box
+    direction="row"
+    alignItems="center"
+    grow={1}
+    shrink={1}
+    {...props}
+    style={[styles.cell, props.style]}
+  />
+);
+
+const columns: ColumnConfig<GetNode<Beneficiaries>, undefined>[] = [
+  {
+    id: "name",
+    width: "grow",
+    title: "Name",
+    renderTitle: ({ title }) => <SimpleHeaderCell text={title} />,
+    renderCell: ({ item }) => (
+      <Cell>
+        <Tag
+          color="gray"
+          icon={match(item.type)
+            .returnType<IconName>()
+            .with("International", () => "earth-regular")
+            .otherwise(() => "person-regular")}
+        />
+
+        <Space width={24} />
+
+        <LakeText variant="medium" color={colors.gray[900]} numberOfLines={1}>
+          {item.label || item.name}
+        </LakeText>
+      </Cell>
+    ),
+  },
+  {
+    id: "identifier",
+    width: "grow",
+    title: "Account identifier",
+    renderTitle: ({ title }) => <SimpleHeaderCell text={title} />,
+    renderCell: ({ item }) => {
+      const [text, value] = match(item)
+        .returnType<[string, string]>()
+        .with({ __typename: "TrustedInternalBeneficiary" }, ({ accountId }) => [
+          "Account ID",
+          accountId,
+        ])
+        .with({ __typename: "TrustedInternationalBeneficiary" }, () => ["TODO", "TODO"])
+        .with({ __typename: "TrustedSepaBeneficiary" }, ({ iban }) => ["IBAN", printFormat(iban)])
+        .otherwise(() => ["", ""]);
+
+      return (
+        <Cell>
+          <LakeText variant="smallRegular" color={colors.gray[400]} numberOfLines={1}>
+            {`${text}: `}
+
+            <LakeText variant="smallMedium" color={colors.gray[700]}>
+              {value}
+            </LakeText>
+          </LakeText>
+        </Cell>
+      );
+    },
+  },
+  {
+    id: "currency",
+    width: 200,
+    title: "Currency",
+    renderTitle: ({ title }) => <SimpleHeaderCell text={title} />,
+    renderCell: ({ item }) => (
+      <Cell>
+        <LakeText variant="smallMedium" color={colors.gray[700]} numberOfLines={1}>
+          {match(item)
+            .with({ __typename: "TrustedInternationalBeneficiary" }, ({ currency }) => currency)
+            .otherwise(() => "EUR") + " "}
+
+          <LakeText variant="smallRegular" color={colors.gray[400]}>
+            (Euro)
+          </LakeText>
+        </LakeText>
+      </Cell>
+    ),
+  },
+  {
+    id: "type",
+    width: 200,
+    title: "Type",
+    renderTitle: ({ title }) => <SimpleHeaderCell text={title} />,
+    renderCell: ({ item }) => (
+      <Cell>
+        <LakeText variant="smallMedium" color={colors.gray[700]} numberOfLines={1}>
+          {match(item.__typename)
+            .with("TrustedInternalBeneficiary", () => "Internal")
+            .with("TrustedInternationalBeneficiary", () => "International")
+            .with("TrustedSepaBeneficiary", () => "SEPA")
+            .otherwise(() => "")}
+        </LakeText>
+      </Cell>
+    ),
+  },
+  // {
+  //   id: "actions",
+  //   width: 48,
+  //   title: "",
+  //   renderTitle: () => null,
+  //   renderCell: ({ isHovered }) => (
+  //     <EndAlignedCell>
+  //       <CellAction>
+  //         <Icon
+  //           name="chevron-right-filled"
+  //           color={isHovered ? colors.gray[900] : colors.gray[500]}
+  //           size={16}
+  //         />
+  //       </CellAction>
+  //     </EndAlignedCell>
+  //   ),
+  // },
+];
+
 const BeneficiaryListImpl = ({
+  large,
   accountMembershipId,
   beneficiaries,
-  large,
+  isLoading,
+  setVariables,
 }: {
+  large: boolean;
   accountMembershipId: string;
   beneficiaries: Beneficiaries;
-  large: boolean;
+  isLoading: boolean;
+  setVariables: (variables: Partial<BeneficiariesListPageQueryVariables>) => void;
 }) => {
   const { edges, pageInfo } = useForwardPagination(beneficiaries);
-  const isEmpty = edges.length === 0;
+  const nodes = useMemo(() => edges.map(edge => edge.node), [edges]);
+  const isEmpty = nodes.length === 0;
 
   const AddButton = (
     <LakeButton
-      // grow={!large}
+      grow={!large}
       icon="add-circle-filled"
       size="small"
       color="current"
@@ -67,14 +206,25 @@ const BeneficiaryListImpl = ({
         </Box>
       )}
 
+      <Space height={24} />
+
       <PlainListView
-        data={edges}
-        keyExtractor={item => item.cursor}
-        headerHeight={0}
-        rowHeight={0}
+        data={nodes}
+        keyExtractor={item => item.id}
+        rowHeight={56}
         groupHeaderHeight={0}
         extraInfo={undefined}
-        columns={[]}
+        columns={columns}
+        headerHeight={48}
+        loading={{
+          isLoading,
+          count: 2,
+        }}
+        onEndReached={() => {
+          if (pageInfo.hasNextPage ?? false) {
+            setVariables({ after: pageInfo.endCursor ?? undefined });
+          }
+        }}
         renderEmptyList={() => (
           <FixedListViewEmpty
             borderedIcon={true}
@@ -100,9 +250,9 @@ export const BeneficiaryList = ({
   accountId: string;
   accountMembershipId: string;
 }) => {
-  const [data] = useQuery(BeneficiariesListPageDocument, {
+  const [data, { isLoading, setVariables }] = useQuery(BeneficiariesListPageDocument, {
     accountId,
-    first: NUM_TO_RENDER, // incorrect typing
+    first: NUM_TO_RENDER,
   });
 
   const beneficiaries = data.mapOkToResult(data =>
@@ -130,9 +280,11 @@ export const BeneficiaryList = ({
           ))
           .with(AsyncData.P.Done(Result.P.Ok(P.select())), beneficiaries => (
             <BeneficiaryListImpl
+              large={large}
               accountMembershipId={accountMembershipId}
               beneficiaries={beneficiaries}
-              large={large}
+              isLoading={isLoading}
+              setVariables={setVariables}
             />
           ))
           .with(AsyncData.P.Done(Result.P.Error(P.select())), error => <ErrorView error={error} />)

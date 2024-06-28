d8e3fc29353c72b091b1fcd4e61f92cbdb727ce4
Mathieu Acthernoene <zoontek@gmail.com>
2024-06-28 15:32:22 +0200

    WIP

--- clients/banking/src/components/BeneficiarySepaWizard.tsx
+++ clients/banking/src/components/BeneficiarySepaWizard.tsx
@@ -1,3 +1,5 @@
+import { Option } from "@swan-io/boxed";
+import { useMutation } from "@swan-io/graphql-client";
 import { LakeButton } from "@swan-io/lake/src/components/LakeButton";
 import { LakeHeading } from "@swan-io/lake/src/components/LakeHeading";
 import { ResponsiveContainer } from "@swan-io/lake/src/components/ResponsiveContainer";
@@ -6,9 +8,14 @@ import { Separator } from "@swan-io/lake/src/components/Separator";
 import { Space } from "@swan-io/lake/src/components/Space";
 import { commonStyles } from "@swan-io/lake/src/constants/commonStyles";
 import { breakpoints, spacings } from "@swan-io/lake/src/constants/design";
+import { showToast } from "@swan-io/lake/src/state/toasts";
+import { filterRejectionsToResult } from "@swan-io/lake/src/utils/gql";
+import { translateError } from "@swan-io/shared-business/src/utils/i18n";
 import { StyleSheet, View } from "react-native";
-import { AccountCountry } from "../graphql/partner";
+import { match } from "ts-pattern";
+import { AccountCountry, AddSepaBeneficiaryDocument } from "../graphql/partner";
 import { t } from "../utils/i18n";
+import { Router } from "../utils/routes";
 import { BeneficiaryWizard } from "./BeneficiaryWizard";
 
 const styles = StyleSheet.create({
@@ -64,6 +71,8 @@ export const BeneficiarySepaWizard = ({
   accountId,
   accountMembershipId,
 }: Props) => {
+  const [addSepaBeneficiary] = useMutation(AddSepaBeneficiaryDocument);
+
   return (
     <ResponsiveContainer style={styles.root} breakpoint={breakpoints.medium}>
       {({ large }) => (
@@ -106,7 +115,30 @@ export const BeneficiarySepaWizard = ({
               accountId={accountId}
               onPressPrevious={onPressClose}
               onPressSubmit={beneficiary => {
-                console.log(beneficiary);
+                addSepaBeneficiary({
+                  input: {
+                    accountId,
+                    iban: beneficiary.iban,
+                    name: beneficiary.name,
+                    consentRedirectUrl:
+                      window.location.origin +
+                      Router.AccountPaymentsBeneficiariesList({ accountMembershipId }),
+                  },
+                })
+                  .mapOk(data => data.addTrustedSepaBeneficiary)
+                  .mapOkToResult(data => Option.fromNullable(data).toResult(data))
+                  .mapOkToResult(filterRejectionsToResult)
+                  .tapOk(({ trustedBeneficiary }) => {
+                    match(trustedBeneficiary.statusInfo)
+                      .with(
+                        { __typename: "TrustedBeneficiaryStatusInfoConsentPending" },
+                        ({ consent }) => window.location.assign(consent.consentUrl),
+                      )
+                      .otherwise(() => {});
+                  })
+                  .tapError(error => {
+                    showToast({ variant: "error", error, title: translateError(error) });
+                  });
               }}
             />
           </ScrollView>

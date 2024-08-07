4b7713f15e7855e35bb8f10e06978964742e5995
Mathieu Acthernoene <zoontek@gmail.com>
2024-08-07 17:38:27 +0200

    Fix style

--- clients/banking/src/components/BeneficiaryDetailTransferList.tsx
+++ clients/banking/src/components/BeneficiaryDetailTransferList.tsx
@@ -2,18 +2,20 @@ import { AsyncData, Option, Result } from "@swan-io/boxed";
 import { useQuery } from "@swan-io/graphql-client";
 import { Box } from "@swan-io/lake/src/components/Box";
 import { Fill } from "@swan-io/lake/src/components/Fill";
-import { FixedListViewEmpty } from "@swan-io/lake/src/components/FixedListView";
+import {
+  FixedListViewEmpty,
+  PlainListViewPlaceholder,
+} from "@swan-io/lake/src/components/FixedListView";
 import { FocusTrapRef } from "@swan-io/lake/src/components/FocusTrap";
 import { FullViewportLayer } from "@swan-io/lake/src/components/FullViewportLayer";
 import { LakeButton } from "@swan-io/lake/src/components/LakeButton";
 import { LakeSearchField } from "@swan-io/lake/src/components/LakeSearchField";
 import { ListRightPanel } from "@swan-io/lake/src/components/ListRightPanel";
-import { LoadingView } from "@swan-io/lake/src/components/LoadingView";
 import { Pressable } from "@swan-io/lake/src/components/Pressable";
 import { ScrollView } from "@swan-io/lake/src/components/ScrollView";
 import { Space } from "@swan-io/lake/src/components/Space";
 import { commonStyles } from "@swan-io/lake/src/constants/commonStyles";
-import { negativeSpacings, spacings } from "@swan-io/lake/src/constants/design";
+import { negativeSpacings } from "@swan-io/lake/src/constants/design";
 import { isNotNullish, nullishOrEmptyToUndefined } from "@swan-io/lake/src/utils/nullish";
 import { omit } from "@swan-io/lake/src/utils/object";
 import { useCallback, useRef, useState } from "react";
@@ -44,14 +46,13 @@ const DEFAULT_STATUSES = [
 ] satisfies TransactionStatus[];
 
 const styles = StyleSheet.create({
-  transactions: {
+  base: {
+    ...commonStyles.fill,
     marginLeft: negativeSpacings[24],
     marginRight: negativeSpacings[20],
   },
   content: {
     ...commonStyles.fill,
-    paddingTop: spacings[16],
-    paddingBottom: spacings[24],
   },
 });
 
@@ -103,161 +104,170 @@ export const BeneficiaryDetailTransferList = ({
     Option.fromNullable(data.trustedBeneficiary?.transactions).toResult(undefined),
   );
 
-  return match(transactions)
-    .with(AsyncData.P.NotAsked, AsyncData.P.Loading, () => <LoadingView />)
-    .with(AsyncData.P.Done(Result.P.Error(P.select())), error => <ErrorView error={error} />)
-    .with(AsyncData.P.Done(Result.P.Ok(P.select())), page => (
-      <>
-        <Space height={24} />
+  return (
+    <>
+      <Space height={24} />
 
-        <Box alignItems="center" direction="row">
-          {transferCreationVisible ? (
-            <LakeButton
-              icon="add-circle-filled"
-              size="small"
-              color="current"
-              onPress={() => {
-                Router.push("AccountPaymentsBeneficiariesDetails", {
-                  ...params,
-                  new:
-                    beneficiary.__typename === "TrustedInternationalBeneficiary"
-                      ? "international"
-                      : "transfer",
-                });
-              }}
-            >
-              {t("common.new")}
-            </LakeButton>
-          ) : (
-            <Fill />
-          )}
-
-          <LakeSearchField
-            initialValue={search ?? ""}
-            placeholder={t("common.search")}
-            onChangeText={search => {
-              Router.replace("AccountPaymentsBeneficiariesDetails", {
+      <Box alignItems="center" direction="row">
+        {transferCreationVisible ? (
+          <LakeButton
+            icon="add-circle-filled"
+            size="small"
+            color="current"
+            onPress={() => {
+              Router.push("AccountPaymentsBeneficiariesDetails", {
                 ...params,
-                search,
+                new:
+                  beneficiary.__typename === "TrustedInternationalBeneficiary"
+                    ? "international"
+                    : "transfer",
               });
             }}
-          />
-        </Box>
+          >
+            {t("common.new")}
+          </LakeButton>
+        ) : (
+          <Fill />
+        )}
 
-        <ScrollView style={styles.transactions} contentContainerStyle={styles.content}>
-          <Connection connection={page}>
-            {transactions => (
-              <>
-                <TransactionList
-                  withStickyTabs={true}
-                  withGrouping={false}
-                  transactions={transactions?.edges ?? []}
-                  renderEmptyList={() =>
-                    hasSearch ? (
-                      <FixedListViewEmpty
-                        icon="lake-transfer"
-                        borderedIcon={true}
-                        title={t("transfer.list.noResults")}
-                        subtitle={t("common.list.noResultsSuggestion")}
-                      />
-                    ) : (
-                      <FixedListViewEmpty
-                        borderedIcon={true}
-                        icon="lake-transfer"
-                        title={t("transfer.list.noResults")}
-                      />
-                    )
-                  }
-                  getRowLink={({ item }) => (
-                    <Pressable onPress={() => setActiveTransactionId(item.id)} />
-                  )}
-                  pageSize={PAGE_SIZE}
-                  activeRowId={activeTransactionId ?? undefined}
-                  onActiveRowChange={onActiveRowChange}
-                  loading={{
-                    isLoading,
-                    count: PAGE_SIZE,
-                  }}
-                  onEndReached={() => {
-                    if (transactions?.pageInfo.hasNextPage ?? false) {
-                      setVariables({
-                        after: transactions?.pageInfo.endCursor ?? undefined,
-                      });
+        <LakeSearchField
+          initialValue={search ?? ""}
+          placeholder={t("common.search")}
+          onChangeText={search => {
+            Router.replace("AccountPaymentsBeneficiariesDetails", {
+              ...params,
+              search,
+            });
+          }}
+        />
+      </Box>
+
+      <ScrollView style={styles.base} contentContainerStyle={styles.content}>
+        {match(transactions)
+          .with(AsyncData.P.NotAsked, AsyncData.P.Loading, () => (
+            <PlainListViewPlaceholder
+              count={PAGE_SIZE}
+              headerHeight={16}
+              rowHeight={72}
+              rowVerticalSpacing={0}
+            />
+          ))
+          .with(AsyncData.P.Done(Result.P.Error(P.select())), error => <ErrorView error={error} />)
+          .with(AsyncData.P.Done(Result.P.Ok(P.select())), transactionsPage => (
+            <Connection connection={transactionsPage}>
+              {transactions => (
+                <>
+                  <Space height={16} />
+
+                  <TransactionList
+                    withStickyTabs={true}
+                    withGrouping={false}
+                    transactions={transactions?.edges ?? []}
+                    renderEmptyList={() =>
+                      hasSearch ? (
+                        <FixedListViewEmpty
+                          icon="lake-transfer"
+                          borderedIcon={true}
+                          title={t("transfer.list.noResults")}
+                          subtitle={t("common.list.noResultsSuggestion")}
+                        />
+                      ) : (
+                        <FixedListViewEmpty
+                          borderedIcon={true}
+                          icon="lake-transfer"
+                          title={t("transfer.list.noResults")}
+                        />
+                      )
                     }
-                  }}
-                />
+                    getRowLink={({ item }) => (
+                      <Pressable onPress={() => setActiveTransactionId(item.id)} />
+                    )}
+                    pageSize={PAGE_SIZE}
+                    activeRowId={activeTransactionId ?? undefined}
+                    onActiveRowChange={onActiveRowChange}
+                    loading={{
+                      isLoading,
+                      count: PAGE_SIZE,
+                    }}
+                    onEndReached={() => {
+                      if (transactions?.pageInfo.hasNextPage ?? false) {
+                        setVariables({
+                          after: transactions?.pageInfo.endCursor ?? undefined,
+                        });
+                      }
+                    }}
+                  />
 
-                <ListRightPanel
-                  ref={panelRef}
-                  keyExtractor={item => item.node.id}
-                  items={transactions?.edges ?? []}
-                  activeId={activeTransactionId}
-                  onActiveIdChange={setActiveTransactionId}
-                  onClose={() => setActiveTransactionId(null)}
-                  previousLabel={t("common.previous")}
-                  nextLabel={t("common.next")}
-                  closeLabel={t("common.closeButton")}
-                  render={({ node }, large) => (
-                    <TransactionDetail
-                      accountMembershipId={params.accountMembershipId}
-                      large={large}
-                      transactionId={node.id}
-                      canQueryCardOnTransaction={canQueryCardOnTransaction}
-                      canViewAccount={canViewAccount}
-                    />
-                  )}
-                />
-              </>
-            )}
-          </Connection>
-        </ScrollView>
+                  <ListRightPanel
+                    ref={panelRef}
+                    keyExtractor={item => item.node.id}
+                    items={transactions?.edges ?? []}
+                    activeId={activeTransactionId}
+                    onActiveIdChange={setActiveTransactionId}
+                    onClose={() => setActiveTransactionId(null)}
+                    previousLabel={t("common.previous")}
+                    nextLabel={t("common.next")}
+                    closeLabel={t("common.closeButton")}
+                    render={({ node }, large) => (
+                      <TransactionDetail
+                        accountMembershipId={params.accountMembershipId}
+                        large={large}
+                        transactionId={node.id}
+                        canQueryCardOnTransaction={canQueryCardOnTransaction}
+                        canViewAccount={canViewAccount}
+                      />
+                    )}
+                  />
+                </>
+              )}
+            </Connection>
+          ))
+          .exhaustive()}
+      </ScrollView>
 
-        {match(beneficiary)
-          .with({ __typename: "TrustedSepaBeneficiary" }, ({ iban, id, name }) => {
+      {match(beneficiary)
+        .with({ __typename: "TrustedSepaBeneficiary" }, ({ iban, id, name }) => (
+          <FullViewportLayer visible={params.new === "transfer"}>
+            <TransferRegularWizard
+              accountCountry={accountCountry}
+              accountId={accountId}
+              accountMembershipId={params.accountMembershipId}
+              canViewAccount={canViewAccount}
+              canManageBeneficiaries={canManageBeneficiaries}
+              initialBeneficiary={{ kind: "saved", iban, id, name }}
+              onPressClose={() => {
+                Router.push("AccountPaymentsBeneficiariesDetails", omit(params, ["new"]));
+              }}
+            />
+          </FullViewportLayer>
+        ))
+        .with(
+          {
+            __typename: "TrustedInternationalBeneficiary",
+            route: P.not("Unknown"),
+            currency: P.when(isSupportedCurrency),
+          },
+          ({ currency, id, name, route, details }) => {
+            const values = details.map(({ key, value }) => ({ key, value })); // remove typenames
+
             return (
-              <FullViewportLayer visible={params.new === "transfer"}>
-                <TransferRegularWizard
-                  accountCountry={accountCountry}
+              <FullViewportLayer visible={params.new === "international"}>
+                <TransferInternationalWizard
                   accountId={accountId}
                   accountMembershipId={params.accountMembershipId}
+                  forcedCurrency={currency}
                   canViewAccount={canViewAccount}
                   canManageBeneficiaries={canManageBeneficiaries}
-                  initialBeneficiary={{ kind: "saved", iban, id, name }}
+                  initialBeneficiary={{ kind: "saved", currency, id, name, route, values }}
                   onPressClose={() => {
                     Router.push("AccountPaymentsBeneficiariesDetails", omit(params, ["new"]));
                   }}
                 />
               </FullViewportLayer>
             );
-          })
-          .with(
-            {
-              __typename: "TrustedInternationalBeneficiary",
-              route: P.not("Unknown"),
-              currency: P.when(isSupportedCurrency),
-            },
-            ({ currency, id, name, route, details }) => {
-              const values = details.map(({ key, value }) => ({ key, value })); // remove typenames
-
-              return (
-                <FullViewportLayer visible={params.new === "international"}>
-                  <TransferInternationalWizard
-                    accountId={accountId}
-                    accountMembershipId={params.accountMembershipId}
-                    forcedCurrency={currency}
-                    canViewAccount={canViewAccount}
-                    canManageBeneficiaries={canManageBeneficiaries}
-                    initialBeneficiary={{ kind: "saved", currency, id, name, route, values }}
-                    onPressClose={() => {
-                      Router.push("AccountPaymentsBeneficiariesDetails", omit(params, ["new"]));
-                    }}
-                  />
-                </FullViewportLayer>
-              );
-            },
-          )
-          .otherwise(() => null)}
-      </>
-    ))
-    .exhaustive();
+          },
+        )
+        .otherwise(() => null)}
+    </>
+  );
 };

ad286075cd6c4582088b1f3ef7c4ea2261fceb8a
bloodyowl <bloodyowl@icloud.com>
2024-08-19 09:52:57 +0200

    Fix identification legacy transition

--- clients/banking/src/utils/identification.ts
+++ clients/banking/src/utils/identification.ts
@@ -1,17 +1,42 @@
 import { P, isMatching, match } from "ts-pattern";
 import { IdentificationFragment, IdentificationLevelFragment } from "../graphql/partner";
 
+const temp__handleLegacyStatus = (
+  identificationLevel: IdentificationLevelFragment,
+): Exclude<
+  IdentificationLevelFragment,
+  { __typename: "NotStartedIdentificationLevelStatusInfo" }
+> => {
+  if (identificationLevel.__typename === "NotStartedIdentificationLevelStatusInfo") {
+    return { __typename: "StartedIdentificationLevelStatusInfo", status: "Started" };
+  } else {
+    return identificationLevel;
+  }
+};
+
 export const getIdentificationLevelStatusInfo = (identification: IdentificationFragment) =>
   match(identification)
-    .returnType<IdentificationLevelFragment>()
-    .with({ process: "Expert", levels: { expert: P.select() } }, statusInfo => statusInfo)
+    .returnType<
+      Exclude<
+        IdentificationLevelFragment,
+        { __typename: "NotStartedIdentificationLevelStatusInfo" }
+      >
+    >()
+    .with({ process: "Expert", levels: { expert: P.select() } }, statusInfo =>
+      temp__handleLegacyStatus(statusInfo),
+    )
     .with(
       {
         process: "QES",
       },
-      ({ levels: { expert, qes } }) => (expert.status === "Valid" ? qes : expert),
+      ({ levels: { expert, qes } }) =>
+        "status" in expert && expert.status === "Valid"
+          ? temp__handleLegacyStatus(qes)
+          : temp__handleLegacyStatus(expert),
     )
-    .with({ process: "PVID", levels: { pvid: P.select() } }, statusInfo => statusInfo)
+    .with({ process: "PVID", levels: { pvid: P.select() } }, statusInfo =>
+      temp__handleLegacyStatus(statusInfo),
+    )
     .otherwise(() => ({
       __typename: "StartedIdentificationLevelStatusInfo",
       status: "Started",

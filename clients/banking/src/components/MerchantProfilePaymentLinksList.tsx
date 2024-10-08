b83b93d9d2e38274e69637ca764935c767a9f799
bloodyowl <bloodyowl@icloud.com>
2024-10-08 17:27:19 +0200

    Add right panel

--- clients/banking/src/components/MerchantProfilePaymentLinksList.tsx
+++ clients/banking/src/components/MerchantProfilePaymentLinksList.tsx
@@ -1,43 +1,25 @@
-import { Future } from "@swan-io/boxed";
-import { Box } from "@swan-io/lake/src/components/Box";
-import { Fill } from "@swan-io/lake/src/components/Fill";
-import { FixedListViewEmpty, LinkConfig } from "@swan-io/lake/src/components/FixedListView";
 import {
   CellAction,
   CopyableRegularTextCell,
   EndAlignedCell,
   SimpleHeaderCell,
   StartAlignedCell,
-} from "@swan-io/lake/src/components/FixedListViewCells";
+} from "@swan-io/lake/src/components/Cells";
+import { EmptyView } from "@swan-io/lake/src/components/EmptyView";
 import { Icon } from "@swan-io/lake/src/components/Icon";
-import { LakeButton } from "@swan-io/lake/src/components/LakeButton";
-import { LakeSearchField } from "@swan-io/lake/src/components/LakeSearchField";
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
 import { ColumnConfig, PlainListView } from "@swan-io/lake/src/components/PlainListView";
-import { Space } from "@swan-io/lake/src/components/Space";
 import { Tag } from "@swan-io/lake/src/components/Tag";
-import { Toggle } from "@swan-io/lake/src/components/Toggle";
+import { LinkConfig } from "@swan-io/lake/src/components/VirtualizedList";
 import { colors, spacings } from "@swan-io/lake/src/constants/design";
-import { nullishOrEmptyToUndefined } from "@swan-io/lake/src/utils/nullish";
-import { ReactElement, useState } from "react";
+import { isNullishOrEmpty } from "@swan-io/lake/src/utils/nullish";
+import { ReactElement } from "react";
 import { StyleSheet, View } from "react-native";
 import { match } from "ts-pattern";
 import { PaymentLinkFragment } from "../graphql/partner";
 import { formatCurrency, t } from "../utils/i18n";
-import { Router } from "../utils/routes";
 
 const styles = StyleSheet.create({
-  filters: {
-    paddingHorizontal: spacings[24],
-    paddingBottom: spacings[12],
-  },
-  filtersLarge: {
-    paddingHorizontal: spacings[40],
-  },
-  endFilters: {
-    flexGrow: 0,
-    flexShrink: 1,
-  },
   cell: {
     display: "flex",
     paddingHorizontal: spacings[16],
@@ -61,29 +43,21 @@ const styles = StyleSheet.create({
   },
 });
 
-type Props = {
-  accountMembershipId: string;
-  merchantProfileId: string;
-  paymentLinks: PaymentLinkFragment[];
-  params: { status: "Active" | "Archived"; search: string };
-  getRowLink: (item: LinkConfig<PaymentLinkFragment, ExtraInfo>) => ReactElement;
-  activeRowId: string | undefined;
-  onActiveRowChange: (element: HTMLElement) => void;
-  onEndReached: () => void;
-  onPressReload: () => Future<unknown>;
-  isLoading: boolean;
-  large: boolean;
-};
-
 type ExtraInfo = undefined;
 
 const PaymentLinkCell = ({ paymentLink }: { paymentLink: PaymentLinkFragment }) => {
   return (
     <View style={[styles.cell, styles.paddedCell]}>
       <View style={styles.transactionSummary}>
-        <LakeText variant="smallRegular" style={styles.overflowingText}>
-          {paymentLink.label}
-        </LakeText>
+        {isNullishOrEmpty(paymentLink.label) ? (
+          <LakeText variant="smallRegular" color={colors.gray[500]}>
+            {"-"}
+          </LakeText>
+        ) : (
+          <LakeText variant="smallRegular" color={colors.gray[900]} style={styles.overflowingText}>
+            {paymentLink.label}
+          </LakeText>
+        )}
 
         <LakeText variant="regular" color={colors.gray[900]}>
           {formatCurrency(Number(paymentLink.amount.value), paymentLink.amount.currency)}
@@ -100,9 +74,15 @@ const columns: ColumnConfig<PaymentLinkFragment, ExtraInfo>[] = [
     renderTitle: ({ title }) => <SimpleHeaderCell text={title} />,
     renderCell: ({ item }) => (
       <StartAlignedCell>
-        <LakeText variant="medium" color={colors.gray[900]}>
-          {item.label}
-        </LakeText>
+        {isNullishOrEmpty(item.label) ? (
+          <LakeText variant="smallRegular" color={colors.gray[300]}>
+            {"-"}
+          </LakeText>
+        ) : (
+          <LakeText variant="medium" color={colors.gray[900]}>
+            {item.label}
+          </LakeText>
+        )}
       </StartAlignedCell>
     ),
   },
@@ -136,13 +116,13 @@ const columns: ColumnConfig<PaymentLinkFragment, ExtraInfo>[] = [
       <EndAlignedCell>
         {match(item.statusInfo.status)
           .with("Active", () => (
-            <Tag color="positive"> {t("merchantProfile.paymentLink.status.active")} </Tag>
+            <Tag color="shakespear"> {t("merchantProfile.paymentLink.status.active")} </Tag>
           ))
           .with("Completed", () => (
-            <Tag color="negative"> {t("merchantProfile.paymentLink.status.completed")} </Tag>
+            <Tag color="positive"> {t("merchantProfile.paymentLink.status.completed")} </Tag>
           ))
           .with("Expired", () => (
-            <Tag color="negative"> {t("merchantProfile.paymentLink.status.expired")} </Tag>
+            <Tag color="gray"> {t("merchantProfile.paymentLink.status.expired")} </Tag>
           ))
           .exhaustive()}
       </EndAlignedCell>
@@ -228,106 +208,51 @@ const smallColumns: ColumnConfig<PaymentLinkFragment, ExtraInfo>[] = [
   },
 ];
 
+type Props = {
+  paymentLinks: PaymentLinkFragment[];
+  getRowLink: (item: LinkConfig<PaymentLinkFragment, ExtraInfo>) => ReactElement;
+  activeRowId: string | undefined;
+  onActiveRowChange: (element: HTMLElement) => void;
+  onEndReached: () => void;
+  isLoading: boolean;
+  large: boolean;
+};
+
 export const MerchantProfilePaymentLinksList = ({
   paymentLinks,
   onEndReached,
-  onPressReload,
-  merchantProfileId,
-  accountMembershipId,
   large,
-  params,
   getRowLink,
   activeRowId,
   onActiveRowChange,
   isLoading,
 }: Props) => {
-  const [isRefreshing, setIsRefreshing] = useState(false);
-
-  const search = nullishOrEmptyToUndefined(params.search);
-
   return (
-    <>
-      <Space height={24} />
-
-      <Box
-        direction="row"
-        alignItems="center"
-        style={[styles.filters, large && styles.filtersLarge]}
-      >
-        <LakeButton
-          ariaLabel={t("common.refresh")}
-          mode="secondary"
-          size="small"
-          icon="arrow-counterclockwise-filled"
-          loading={isRefreshing}
-          onPress={() => {
-            setIsRefreshing(true);
-            onPressReload().tap(() => setIsRefreshing(false));
-          }}
+    <PlainListView
+      withoutScroll={!large}
+      data={paymentLinks}
+      keyExtractor={item => item.id}
+      headerHeight={48}
+      rowHeight={56}
+      groupHeaderHeight={48}
+      extraInfo={undefined}
+      columns={columns}
+      smallColumns={smallColumns}
+      getRowLink={getRowLink}
+      onActiveRowChange={onActiveRowChange}
+      activeRowId={activeRowId}
+      onEndReached={onEndReached}
+      loading={{
+        isLoading,
+        count: 5,
+      }}
+      renderEmptyList={() => (
+        <EmptyView
+          icon="lake-transfer"
+          borderedIcon={true}
+          title={t("merchantProfile.paymentLinks.noResults")}
         />
-
-        <Fill minWidth={16} />
-
-        <Box direction="row" alignItems="center" justifyContent="end" style={styles.endFilters}>
-          <Toggle
-            mode={large ? "desktop" : "mobile"}
-            value={params.status === "Active"}
-            onToggle={status =>
-              Router.push("AccountMerchantsProfilePaymentLinkList", {
-                accountMembershipId,
-                merchantProfileId,
-                status: status ? "Active" : "Archived",
-              })
-            }
-            onLabel={t("merchantProfile.list.Active")}
-            offLabel={t("merchantProfile.list.Inactive")}
-          />
-
-          <Fill minWidth={16} />
-
-          <LakeSearchField
-            initialValue={search ?? ""}
-            placeholder={t("common.search")}
-            onChangeText={search => {
-              Router.replace("AccountMerchantsProfilePaymentLinkList", {
-                accountMembershipId,
-                merchantProfileId,
-                ...params,
-                search,
-              });
-            }}
-          />
-        </Box>
-      </Box>
-
-      <Space height={24} />
-
-      <PlainListView
-        withoutScroll={!large}
-        data={paymentLinks}
-        keyExtractor={item => item.id}
-        headerHeight={48}
-        rowHeight={56}
-        groupHeaderHeight={48}
-        extraInfo={undefined}
-        columns={columns}
-        smallColumns={smallColumns}
-        getRowLink={getRowLink}
-        onActiveRowChange={onActiveRowChange}
-        activeRowId={activeRowId}
-        onEndReached={onEndReached}
-        loading={{
-          isLoading,
-          count: 5,
-        }}
-        renderEmptyList={() => (
-          <FixedListViewEmpty
-            icon="lake-transfer"
-            borderedIcon={true}
-            title={t("merchantProfile.paymentLinks.noResults")}
-          />
-        )}
-      />
-    </>
+      )}
+    />
   );
 };

e8863ac449b757a87f7b43ec03752bc2801488e2
bloodyowl <bloodyowl@icloud.com>
2024-11-29 18:00:42 +0100

    Add identificationLevel=Auto on mail invitations

--- clients/banking/src/components/MembershipInvitationLinkModal.tsx
+++ clients/banking/src/components/MembershipInvitationLinkModal.tsx
@@ -1,8 +1,9 @@
-import { Option } from "@swan-io/boxed";
+import { AsyncData, Option, Result } from "@swan-io/boxed";
 import { useDeferredQuery } from "@swan-io/graphql-client";
 import { Box } from "@swan-io/lake/src/components/Box";
 import { LakeLabel } from "@swan-io/lake/src/components/LakeLabel";
 import { LakeTextInput } from "@swan-io/lake/src/components/LakeTextInput";
+import { LoadingView } from "@swan-io/lake/src/components/LoadingView";
 import { Space } from "@swan-io/lake/src/components/Space";
 import { LakeModal } from "@swan-io/shared-business/src/components/LakeModal";
 import { useEffect } from "react";
@@ -12,6 +13,7 @@ import { getMemberName } from "../utils/accountMembership";
 import { t } from "../utils/i18n";
 import { projectConfiguration } from "../utils/projectId";
 import { CopyTextButton } from "./CopyTextButton";
+import { ErrorView } from "./ErrorView";
 
 type Props = {
   accountMembershipId: string | undefined;
@@ -26,14 +28,6 @@ export const MembershipInvitationLinkModal = ({ accountMembershipId, onPressClos
       return () => request.cancel();
     }
   }, [accountMembershipId, query]);
-
-  const value = match(projectConfiguration)
-    .with(
-      Option.P.Some({ projectId: P.select(), mode: "MultiProject" }),
-      projectId =>
-        `${__env.BANKING_URL}/api/projects/${projectId}/invitation/${accountMembershipId ?? ""}`,
-    )
-    .otherwise(() => `${__env.BANKING_URL}/api/invitation/${accountMembershipId ?? ""}`);
 
   return (
     <LakeModal
@@ -51,18 +45,54 @@ export const MembershipInvitationLinkModal = ({ accountMembershipId, onPressClos
         )
         .getOr(t("members.invitationTitle"))}
     >
-      <LakeLabel
-        label={t("members.invitationLink")}
-        render={id => {
+      {match(data)
+        .with(AsyncData.P.NotAsked, AsyncData.P.Loading, () => <LoadingView />)
+        .with(AsyncData.P.Done(Result.P.Error(P.select())), () => <ErrorView error={error} />)
+        .with(AsyncData.P.Done(Result.P.Ok(P.select())), ({ accountMembership }) => {
+          const value = match(projectConfiguration)
+            .with(
+              Option.P.Some({ projectId: P.select(), mode: "MultiProject" }),
+              projectId =>
+                `${__env.BANKING_URL}/api/projects/${projectId}/invitation/${accountMembershipId ?? ""}`,
+            )
+            .otherwise(() => `${__env.BANKING_URL}/api/invitation/${accountMembershipId ?? ""}`);
+
+          const url = new URL(value);
+          url.searchParams.append("identificationLevel", "Auto");
+          match(accountMembership.statusInfo)
+            .with(
+              {
+                __typename: P.union(
+                  "AccountMembershipBindingUserErrorStatusInfo",
+                  "AccountMembershipInvitationSentStatusInfo",
+                ),
+              },
+              ({ restrictedTo }) => {
+                if (restrictedTo.phoneNumber == null && accountMembership?.email != null) {
+                  url.searchParams.append("email", accountMembership.email);
+                }
+              },
+            )
+            .otherwise(() => {});
+
+          const urlAsString = url.toString();
+
           return (
-            <Box direction="row">
-              <LakeTextInput id={id} readOnly={true} value={value} hideErrors={true} />
-              <Space width={12} />
-              <CopyTextButton value={value} />
-            </Box>
+            <LakeLabel
+              label={t("members.invitationLink")}
+              render={id => {
+                return (
+                  <Box direction="row">
+                    <LakeTextInput id={id} readOnly={true} value={urlAsString} hideErrors={true} />
+                    <Space width={12} />
+                    <CopyTextButton value={urlAsString} />
+                  </Box>
+                );
+              }}
+            />
           );
-        }}
-      />
+        })
+        .exhaustive()}
     </LakeModal>
   );
 };

c0a46ec399e5106f0360c423646fc4f24199064e
Mathieu Acthernoene <zoontek@gmail.com>
2024-11-29 16:21:08 +0100

    Add promise-all-settled polyfill (#1039)

--- clients/banking/src/index.tsx
+++ clients/banking/src/index.tsx
@@ -5,6 +5,7 @@ import { ResizeObserver } from "@juggle/resize-observer";
 import "core-js/proposals/array-flat-map";
 import "core-js/proposals/change-array-by-copy-stage-4";
 import "core-js/proposals/object-from-entries";
+import "core-js/proposals/promise-all-settled";
 import "core-js/proposals/relative-indexing-method";
 import "core-js/proposals/string-replace-all-stage-4";
 

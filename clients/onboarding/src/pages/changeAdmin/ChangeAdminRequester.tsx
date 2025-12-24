f46035aef5ab96028ed8daa9a5381c9017ed0f1d
Frederic <32013054+Epimodev@users.noreply.github.com>
2025-12-24 14:11:05 +0100

    Change account admin setup title (#1351)
    * fix stepper steps
    
    * set title and description for each pages
    
    * translate with ai

--- clients/onboarding/src/pages/changeAdmin/ChangeAdminRequester.tsx
+++ clients/onboarding/src/pages/changeAdmin/ChangeAdminRequester.tsx
@@ -1,7 +1,13 @@
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
+import { ResponsiveContainer } from "@swan-io/lake/src/components/ResponsiveContainer";
+import { Space } from "@swan-io/lake/src/components/Space";
+import { Tile } from "@swan-io/lake/src/components/Tile";
+import { breakpoints } from "@swan-io/lake/src/constants/design";
 import { OnboardingFooter } from "../../components/OnboardingFooter";
 import { OnboardingStepContent } from "../../components/OnboardingStepContent";
 import { PartnershipFooter } from "../../components/PartnershipFooter";
+import { StepTitle } from "../../components/StepTitle";
+import { t } from "../../utils/i18n";
 import { ChangeAdminRoute, Router } from "../../utils/routes";
 
 type Props = {
@@ -21,7 +27,20 @@ export const ChangeAdminRequester = ({ changeAdminRequestId, previousStep, nextS
 
   return (
     <OnboardingStepContent>
-      <LakeText>ChangeAdminRequester</LakeText>
+      <ResponsiveContainer breakpoint={breakpoints.medium}>
+        {({ small }) => (
+          <>
+            <StepTitle isMobile={small}>{t("changeAdmin.step.requesterInfo.title")}</StepTitle>
+            <Space height={small ? 8 : 12} />
+            <LakeText>{t("changeAdmin.step.requesterInfo.description")}</LakeText>
+            <Space height={small ? 24 : 32} />
+
+            <Tile>
+              <LakeText>ChangeAdminRequester</LakeText>
+            </Tile>
+          </>
+        )}
+      </ResponsiveContainer>
 
       <OnboardingFooter onPrevious={onPressPrevious} onNext={onPressNext} loading={false} />
       <PartnershipFooter />

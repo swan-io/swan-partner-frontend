3f6723dd21a287bc775d5714f19a1391b512b803
Frederic <frederic.godin@swan.io>
2025-12-24 11:48:41 +0100

    set title and description for each pages

--- clients/onboarding/src/pages/changeAdmin/ChangeAdminConfirm.tsx
+++ clients/onboarding/src/pages/changeAdmin/ChangeAdminConfirm.tsx
@@ -1,7 +1,13 @@
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
+import { ResponsiveContainer } from "@swan-io/lake/src/components/ResponsiveContainer";
+import { Space } from "@swan-io/lake/src/components/Space";
+import { Tile } from "@swan-io/lake/src/components/Tile";
+import { breakpoints } from "@swan-io/lake/src/constants/design";
 import { OnboardingFooter } from "../../components/OnboardingFooter";
 import { OnboardingStepContent } from "../../components/OnboardingStepContent";
 import { PartnershipFooter } from "../../components/PartnershipFooter";
+import { StepTitle } from "../../components/StepTitle";
+import { t } from "../../utils/i18n";
 import { ChangeAdminRoute, Router } from "../../utils/routes";
 
 type Props = {
@@ -20,7 +26,20 @@ export const ChangeAdminConfirm = ({ changeAdminRequestId, previousStep }: Props
 
   return (
     <OnboardingStepContent>
-      <LakeText>ChangeAdminConfirm</LakeText>
+      <ResponsiveContainer breakpoint={breakpoints.medium}>
+        {({ small }) => (
+          <>
+            <StepTitle isMobile={small}>{t("changeAdmin.step.confirm.title")}</StepTitle>
+            <Space height={small ? 8 : 12} />
+            <LakeText>{t("changeAdmin.step.confirm.description")}</LakeText>
+            <Space height={small ? 24 : 32} />
+
+            <Tile>
+              <LakeText>ChangeAdminConfirm</LakeText>
+            </Tile>
+          </>
+        )}
+      </ResponsiveContainer>
 
       <OnboardingFooter onPrevious={onPressPrevious} onNext={onSubmit} loading={false} />
       <PartnershipFooter />
--- clients/onboarding/src/pages/changeAdmin/ChangeAdminContext1.tsx
+++ clients/onboarding/src/pages/changeAdmin/ChangeAdminContext1.tsx
@@ -1,7 +1,13 @@
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
+import { ResponsiveContainer } from "@swan-io/lake/src/components/ResponsiveContainer";
+import { Space } from "@swan-io/lake/src/components/Space";
+import { Tile } from "@swan-io/lake/src/components/Tile";
+import { breakpoints } from "@swan-io/lake/src/constants/design";
 import { OnboardingFooter } from "../../components/OnboardingFooter";
 import { OnboardingStepContent } from "../../components/OnboardingStepContent";
 import { PartnershipFooter } from "../../components/PartnershipFooter";
+import { StepTitle } from "../../components/StepTitle";
+import { t } from "../../utils/i18n";
 import { ChangeAdminRoute, Router } from "../../utils/routes";
 
 type Props = {
@@ -16,9 +22,26 @@ export const ChangeAdminContext1 = ({ changeAdminRequestId, nextStep }: Props) =
 
   return (
     <OnboardingStepContent>
-      <LakeText>ChangeAdminContext1</LakeText>
+      <ResponsiveContainer breakpoint={breakpoints.medium}>
+        {({ small }) => (
+          <>
+            <StepTitle isMobile={small}>{t("changeAdmin.step.context1.title")}</StepTitle>
+            <Space height={small ? 8 : 12} />
+            <LakeText>{t("changeAdmin.step.context1.description")}</LakeText>
+            <Space height={small ? 24 : 32} />
 
-      <OnboardingFooter onNext={onPressNext} loading={false} />
+            <Tile>
+              <LakeText>ChangeAdminContext1</LakeText>
+            </Tile>
+          </>
+        )}
+      </ResponsiveContainer>
+
+      <OnboardingFooter
+        nextLabel="changeAdmin.step.context1.continue"
+        onNext={onPressNext}
+        loading={false}
+      />
       <PartnershipFooter />
     </OnboardingStepContent>
   );

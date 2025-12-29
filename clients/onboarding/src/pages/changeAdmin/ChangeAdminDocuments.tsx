daf8ba0b1130d630b5ec30f893522c2759f12075
Frederic <32013054+Epimodev@users.noreply.github.com>
2025-12-29 14:20:34 +0100

    change account admin documents step (#1357)
    * change admin document step
    
    * set template language

--- clients/onboarding/src/pages/changeAdmin/ChangeAdminDocuments.tsx
+++ clients/onboarding/src/pages/changeAdmin/ChangeAdminDocuments.tsx
@@ -1,29 +1,112 @@
+import { useMutation } from "@swan-io/graphql-client";
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
 import { ResponsiveContainer } from "@swan-io/lake/src/components/ResponsiveContainer";
 import { Space } from "@swan-io/lake/src/components/Space";
 import { Tile } from "@swan-io/lake/src/components/Tile";
 import { breakpoints } from "@swan-io/lake/src/constants/design";
+import { filterRejectionsToResult } from "@swan-io/lake/src/utils/gql";
+import {
+  Document,
+  SupportingDocumentCollection,
+  SupportingDocumentCollectionRef,
+} from "@swan-io/shared-business/src/components/SupportingDocumentCollection";
+import { SwanFile } from "@swan-io/shared-business/src/utils/SwanFile";
+import { ReactNode, useCallback, useRef } from "react";
 import { OnboardingFooter } from "../../components/OnboardingFooter";
 import { OnboardingStepContent } from "../../components/OnboardingStepContent";
 import { PartnershipFooter } from "../../components/PartnershipFooter";
 import { StepTitle } from "../../components/StepTitle";
+import {
+  DeleteSupportingDocumentDocument,
+  GenerateSupportingDocumentUploadUrlDocument,
+  SupportingDocumentCollectionStatus,
+  SupportingDocumentPurposeEnum,
+} from "../../graphql/partner";
 import { t } from "../../utils/i18n";
 import { ChangeAdminRoute, Router } from "../../utils/routes";
 
 type Props = {
+  templateLanguage: string;
   changeAdminRequestId: string;
   previousStep: ChangeAdminRoute;
   nextStep: ChangeAdminRoute;
 };
 
-export const ChangeAdminDocuments = ({ changeAdminRequestId, previousStep, nextStep }: Props) => {
+const DocumentsStepTile = ({ small, children }: { small: boolean; children: ReactNode }) => {
+  if (small) {
+    return <>{children}</>;
+  }
+  return <Tile>{children}</Tile>;
+};
+
+export const ChangeAdminDocuments = ({
+  templateLanguage,
+  changeAdminRequestId,
+  previousStep,
+  nextStep,
+}: Props) => {
+  const [generateSupportingDocumentUploadUrl] = useMutation(
+    GenerateSupportingDocumentUploadUrlDocument,
+  );
+  const [deleteSupportingDocument] = useMutation(DeleteSupportingDocumentDocument);
+
+  const supportingDocumentCollectionRef =
+    useRef<SupportingDocumentCollectionRef<SupportingDocumentPurposeEnum>>(null);
+
   const onPressPrevious = () => {
     Router.push(previousStep, { requestId: changeAdminRequestId });
   };
 
   const onPressNext = () => {
+    const supportingDocumentCollection = supportingDocumentCollectionRef.current;
+    if (supportingDocumentCollection == null) {
+      return;
+    }
+    // Show some validation errors
+    // if (!supportingDocumentCollection.areAllRequiredDocumentsFilled()) {
+    //   return;
+    // }
+
     Router.push(nextStep, { requestId: changeAdminRequestId });
   };
+
+  const generateUpload = ({
+    fileName,
+    purpose,
+  }: {
+    fileName: string;
+    purpose: SupportingDocumentPurposeEnum;
+  }) => {
+    return generateSupportingDocumentUploadUrl({
+      input: {
+        supportingDocumentCollectionId: "", // TODO get value from backend
+        supportingDocumentPurpose: purpose,
+        filename: fileName,
+      },
+    })
+      .mapOk(data => data.generateSupportingDocumentUploadUrl)
+      .mapOkToResult(filterRejectionsToResult)
+      .mapOk(({ upload, supportingDocumentId }) => ({ upload, id: supportingDocumentId }));
+  };
+
+  const onRemoveFile = useCallback(
+    (file: SwanFile) => {
+      return deleteSupportingDocument({ input: { id: file.id } })
+        .mapOk(data => data.deleteSupportingDocument)
+        .mapOkToResult(filterRejectionsToResult);
+    },
+    [deleteSupportingDocument],
+  );
+
+  const docs: Document<SupportingDocumentPurposeEnum>[] = []; // TODO fetch from backend
+  const requiredDocumentsPurposes: SupportingDocumentPurposeEnum[] = [
+    "ProofOfIdentity",
+    "PowerOfAttorney",
+    "LegalRepresentativeProofOfIdentity",
+    "CompanyRegistration",
+  ]; // TODO fetch from backend
+  const supportingDocumentCollectionStatus: SupportingDocumentCollectionStatus =
+    "WaitingForDocument"; // TODO fetch from backend
 
   return (
     <OnboardingStepContent>
@@ -35,9 +118,17 @@ export const ChangeAdminDocuments = ({ changeAdminRequestId, previousStep, nextS
             <LakeText>{t("changeAdmin.step.documents.description")}</LakeText>
             <Space height={small ? 24 : 32} />
 
-            <Tile>
-              <LakeText>ChangeAdminDocuments</LakeText>
-            </Tile>
+            <DocumentsStepTile small={small}>
+              <SupportingDocumentCollection
+                ref={supportingDocumentCollectionRef}
+                documents={docs}
+                requiredDocumentPurposes={requiredDocumentsPurposes}
+                generateUpload={generateUpload}
+                status={supportingDocumentCollectionStatus}
+                templateLanguage={templateLanguage}
+                onRemoveFile={onRemoveFile}
+              />
+            </DocumentsStepTile>
           </>
         )}
       </ResponsiveContainer>

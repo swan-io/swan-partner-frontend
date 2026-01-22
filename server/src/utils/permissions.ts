cf8652d483fd81506702f40faed197cfa60efac7
Frederic <32013054+Epimodev@users.noreply.github.com>
2026-01-22 16:56:17 +0100

    fix permission to allow sending membership invitation (#1405)

--- server/src/utils/permissions.ts
+++ server/src/utils/permissions.ts
@@ -11,7 +11,6 @@ const PERMISSIONS_MATRIX = {
   sendAccountMembershipInviteNotification: {
     settings: {
       canViewMembers: true,
-      canAddNewMembers: true,
     },
   },
   updateAccountMembership: {

0de051b3d16cc1250674eb6458cd4ac36cb13669
Thibaut Baillet <thibaut.baillet@swan.io>
2026-02-10 10:02:43 +0100

    Remove field

--- clients/onboarding/src/pages/v2/individual/OnboardingIndividualFinalize.tsx
+++ clients/onboarding/src/pages/v2/individual/OnboardingIndividualFinalize.tsx
@@ -11,12 +11,8 @@ import { BorderedIcon } from "@swan-io/lake/src/components/BorderedIcon";
 import { Box } from "@swan-io/lake/src/components/Box";
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
 import { Space } from "@swan-io/lake/src/components/Space";
-import { getCountryByCCA3 } from "@swan-io/shared-business/src/constants/countries";
-import { useForm } from "@swan-io/use-form";
 import { match, P } from "ts-pattern";
-import { InputPhoneNumber } from "../../../components/InputPhoneNumber";
 import { env } from "../../../utils/env";
-import { prefixPhoneNumber } from "../../../utils/phone";
 import { projectConfiguration } from "../../../utils/projectId";
 import { Router } from "../../../utils/routes";
 
@@ -26,63 +22,24 @@ type Props = {
 
 export const OnboardingIndividualFinalize = ({ onboarding }: Props) => {
   const onboardingId = onboarding.id;
-  const { accountInfo } = onboarding;
-
-  const { Field, submitForm } = useForm({
-    phoneNumber: {
-      initialValue: {
-        country: getCountryByCCA3(accountInfo?.country ?? "FRA"),
-        nationalNumber: "",
-      },
-      sanitize: ({ country, nationalNumber }) => ({
-        country,
-        nationalNumber: nationalNumber.trim(),
-      }),
-      validate: ({ country, nationalNumber }) => {
-        if (nationalNumber.trim() === "") {
-          return t("error.requiredField");
-        }
-        const phoneNumber = prefixPhoneNumber(country, nationalNumber);
-
-        if (!phoneNumber.valid) {
-          return t("error.invalidPhoneNumber");
-        }
-      },
-    },
-  });
 
   const onPressPrevious = () => {
     Router.push("Activity", { onboardingId });
   };
 
   const onPressNext = () => {
-    submitForm({
-      onSuccess: values => {
-        const option = Option.allFromDict(values);
-        if (option.isNone()) {
-          return;
-        }
+    const queryString = new URLSearchParams();
+    queryString.append("identificationLevel", "Auto");
+    queryString.append("onboardingId", onboardingId);
+    queryString.append("onboardingV2", "true");
 
-        const queryString = new URLSearchParams();
-        queryString.append("identificationLevel", "Auto");
-        queryString.append("onboardingId", onboardingId);
-        queryString.append("onboardingV2", "true");
+    match(projectConfiguration)
+      .with(Option.P.Some({ projectId: P.select(), mode: "MultiProject" }), projectId =>
+        queryString.append("projectId", projectId),
+      )
+      .otherwise(() => {});
 
-        const { phoneNumber } = option.get();
-        const maybePhoneNumber = prefixPhoneNumber(phoneNumber.country, phoneNumber.nationalNumber);
-        if (maybePhoneNumber.valid) {
-          queryString.append("phoneNumber", maybePhoneNumber.e164);
-        }
-
-        match(projectConfiguration)
-          .with(Option.P.Some({ projectId: P.select(), mode: "MultiProject" }), projectId =>
-            queryString.append("projectId", projectId),
-          )
-          .otherwise(() => {});
-
-        window.location.assign(`${env.BANKING_URL}/auth/login?${queryString.toString()}`);
-      },
-    });
+    window.location.assign(`${env.BANKING_URL}/auth/login?${queryString.toString()}`);
   };
 
   return (
@@ -96,20 +53,6 @@ export const OnboardingIndividualFinalize = ({ onboarding }: Props) => {
                 <Space height={24} />
                 <StepTitle isMobile={small}>{t("individual.step.finalize.title")}</StepTitle>
                 <LakeText>{t("individual.step.finalize.content")}</LakeText>
-
-                <Field name="phoneNumber">
-                  {({ value, onBlur, onChange, valid, error, ref }) => (
-                    <InputPhoneNumber
-                      label={""}
-                      ref={ref}
-                      error={error}
-                      value={value}
-                      valid={valid}
-                      onBlur={onBlur}
-                      onValueChange={onChange}
-                    />
-                  )}
-                </Field>
               </Box>
             </Tile>
           </>

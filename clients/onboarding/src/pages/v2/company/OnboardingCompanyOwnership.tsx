227e5ef3da0dce37645c6f6931879d6253bb79a2
Thibaut Baillet <thibaut.baillet@swan.io>
2026-02-24 09:42:01 +0100

    Clean page ownership and translation

--- clients/onboarding/src/pages/v2/company/OnboardingCompanyOwnership.tsx
+++ clients/onboarding/src/pages/v2/company/OnboardingCompanyOwnership.tsx
@@ -1,28 +1,16 @@
-import { Option } from "@swan-io/boxed";
-import { useMutation } from "@swan-io/graphql-client";
 import { ResponsiveContainer } from "@swan-io/lake/src/components/ResponsiveContainer";
 import { Tile } from "@swan-io/lake/src/components/Tile";
 import { breakpoints, colors, radii } from "@swan-io/lake/src/constants/design";
-import { filterRejectionsToResult } from "@swan-io/lake/src/utils/gql";
-import { useForm } from "@swan-io/use-form";
 import { Pressable, StyleSheet } from "react-native";
 import { OnboardingFooter } from "../../../components/OnboardingFooter";
 import { StepTitle } from "../../../components/StepTitle";
-import {
-  CompanyOnboardingFragment,
-  UpdatePublicCompanyAccountHolderOnboardingDocument,
-} from "../../../graphql/partner";
-import { t } from "../../../utils/i18n";
+import { CompanyOnboardingFragment } from "../../../graphql/partner";
+import { formatNestedMessage, t } from "../../../utils/i18n";
 
 import { Icon } from "@swan-io/lake/src/components/Icon";
 import { LakeText } from "@swan-io/lake/src/components/LakeText";
 import { Space } from "@swan-io/lake/src/components/Space";
-import { noop } from "@swan-io/lake/src/utils/function";
-import { showToast } from "@swan-io/shared-business/src/state/toasts";
-import { match } from "ts-pattern";
 import { Router } from "../../../utils/routes";
-import { getUpdateOnboardingError } from "../../../utils/templateTranslations";
-import { badUserInputErrorPattern } from "../../../utils/validation";
 
 export type ActivityFieldName =
   | "businessActivityDescription"
@@ -47,69 +35,28 @@ const styles = StyleSheet.create({
     gap: "16px 32px",
   },
   addOwnerButton: {
-    padding: 18,
+    padding: 24,
     borderColor: colors.gray[300],
     borderWidth: 1,
     borderRadius: radii[8],
     borderStyle: "dashed",
     flexGrow: 1,
+    flexDirection: "row",
     justifyContent: "center",
-    alignItems: "center",
+    gap: "8px",
   },
-  additionalDescription: { fontStyle: "italic" },
 });
 
 export const OnboardingCompanyOwnership = ({ onboarding }: Props) => {
-  const [updateCompanyOnboarding, updateResult] = useMutation(
-    UpdatePublicCompanyAccountHolderOnboardingDocument,
-  );
-
   const onboardingId = onboarding.id;
   // const { company } = onboarding;
-
-  const { submitForm } = useForm({});
 
   const onPressPrevious = () => {
     Router.push("Organisation", { onboardingId });
   };
 
   const onPressNext = () => {
-    submitForm({
-      onSuccess: values => {
-        const option = Option.allFromDict(values);
-        if (option.isNone()) {
-          return;
-        }
-        const currentValues = option.get();
-
-        updateCompanyOnboarding({
-          input: {
-            onboardingId,
-            company: {
-              ...currentValues,
-            },
-          },
-        })
-          .mapOk(data => data.updatePublicCompanyAccountHolderOnboarding)
-          .mapOkToResult(filterRejectionsToResult)
-          .tapOk(() => Router.push("Finalize", { onboardingId }))
-          .tapError(error => {
-            match(error)
-              .with(badUserInputErrorPattern, () => {
-                // const invalidFields = extractServerValidationFields(fields, path => {
-                //   return match(path).otherwise(() => null);
-                // });
-                // invalidFields.forEach(({ fieldName, code }) => {
-                //   const message = getValidationErrorMessage(code, currentValues[fieldName]);
-                //   setFieldError(fieldName, message);
-                // });
-              })
-              .otherwise(noop);
-
-            showToast({ variant: "error", error, ...getUpdateOnboardingError(error) });
-          });
-      },
-    });
+    Router.push("Finalize", { onboardingId });
   };
 
   return (
@@ -118,13 +65,19 @@ export const OnboardingCompanyOwnership = ({ onboarding }: Props) => {
         {({ small }) => (
           <>
             <Tile>
-              <StepTitle isMobile={small}>{t("company.step.owners.title")}</StepTitle>
+              <StepTitle isMobile={small}>{t("company.step.ownersip.title")}</StepTitle>
               <Space height={12} />
-              <LakeText>{t("company.step.owners.description")}</LakeText>
+              <LakeText>{t("company.step.ownersip.description")}</LakeText>
               <Space height={24} />
 
-              <LakeText style={styles.additionalDescription}>
-                {t("company.step.owners.additionalDescription")}
+              <LakeText>
+                {formatNestedMessage("company.step.ownersip.additionalDescription", {
+                  bold: text => (
+                    <LakeText variant="semibold" color={colors.gray[900]}>
+                      {text}
+                    </LakeText>
+                  ),
+                })}
               </LakeText>
 
               <Space height={24} />
@@ -134,20 +87,14 @@ export const OnboardingCompanyOwnership = ({ onboarding }: Props) => {
                 style={styles.addOwnerButton}
                 onPress={() => console.log("open modal")}
               >
-                <Icon name="add-circle-regular" size={32} color={colors.gray[500]} />
-                <Space height={8} />
-                <LakeText>{t("company.step.owners.addTitle")}</LakeText>
+                <Icon name="add-circle-regular" size={24} color={colors.gray[500]} />
+                <LakeText>{t("company.step.ownersip.addTitle")}</LakeText>
               </Pressable>
             </Tile>
           </>
         )}
       </ResponsiveContainer>
-      <OnboardingFooter
-        onNext={onPressNext}
-        onPrevious={onPressPrevious}
-        justifyContent="start"
-        loading={updateResult.isLoading()}
-      />
+      <OnboardingFooter onNext={onPressNext} onPrevious={onPressPrevious} justifyContent="start" />
     </>
   );
 };

a4b0466ad8047f7e1a1aa187e1653b859e8ad3e1
Frederic <32013054+Epimodev@users.noreply.github.com>
2026-03-04 17:52:16 +0100

    refactor remaining spending limit (#1483)
    * refactor remaining spending limit
    
    * improve translations with Gemini

--- clients/banking/src/components/CardItemPhysicalDetails.tsx
+++ clients/banking/src/components/CardItemPhysicalDetails.tsx
@@ -50,7 +50,6 @@ import {
   ConfirmPhysicalCardRenewalDocument,
   PrintPhysicalCardDocument,
   ResumePhysicalCardDocument,
-  SpendingLimitFragment,
   SuspendPhysicalCardDocument,
   ViewPhysicalCardNumbersDocument,
   ViewPhysicalCardPinDocument,
@@ -58,12 +57,12 @@ import {
 import { usePermissions } from "../hooks/usePermissions";
 import { getMemberName } from "../utils/accountMembership";
 import { partnerClient } from "../utils/gql";
-import { formatCurrency, formatNestedMessage, locale, t, translateDay } from "../utils/i18n";
+import { formatCurrency, formatNestedMessage, t } from "../utils/i18n";
 import { Router } from "../utils/routes";
-import { getMonthlySpendingDate } from "../utils/spendingLimit";
 import { Address } from "./CardItemPhysicalDeliveryAddressForm";
 import { CardItemPhysicalDeliveryWizard } from "./CardItemPhysicalDeliveryWizard";
 import { CardItemPhysicalRenewalWizard } from "./CardItemPhysicalRenewalWizard";
+import { RemainingSpendingLimit } from "./CardItemSpendingLimit";
 import { MaskedCard } from "./MaskedCard";
 
 const styles = StyleSheet.create({
@@ -1098,57 +1097,11 @@ export const CardItemPhysicalDetails = ({
                         if (spendingLimit == null) {
                           return null;
                         }
+
                         const spentOverLimitRatio = Math.min(
                           Number(spending.amount.value) / Number(spendingLimit.amount.value),
                           1,
                         );
-                        const remainderToSpend = Math.max(
-                          0,
-                          Number(spendingLimit.amount.value) - Number(spending.amount.value),
-                        );
-
-                        const getSpendingLimitCalendar = (
-                          mode: NonNullable<SpendingLimitFragment["mode"]>,
-                        ) =>
-                          match(mode)
-                            .with({ __typename: "SpendingLimitCalendarDayMode" }, mode => (
-                              <LakeText color={textColor} variant="smallRegular">
-                                {t("card.spendingLimit.calendar.daily", {
-                                  amount: formatCurrency(
-                                    Number(spendingLimit.amount.value),
-                                    spendingLimit.amount.currency,
-                                  ),
-                                  period: spendingLimit.period,
-                                  hour: mode.startHour,
-                                })}
-                              </LakeText>
-                            ))
-                            .with({ __typename: "SpendingLimitCalendarMonthMode" }, mode => (
-                              <LakeText color={textColor} variant="smallRegular">
-                                {t("card.spendingLimit.calendar.monthly", {
-                                  amount: formatCurrency(
-                                    Number(spendingLimit.amount.value),
-                                    spendingLimit.amount.currency,
-                                  ),
-                                  period: spendingLimit.period,
-                                  day: getMonthlySpendingDate(mode.startMonthDay, mode.startHour),
-                                })}
-                              </LakeText>
-                            ))
-                            .with({ __typename: "SpendingLimitCalendarWeekMode" }, mode => (
-                              <LakeText color={textColor} variant="smallRegular">
-                                {t("card.spendingLimit.calendar.weekly", {
-                                  amount: formatCurrency(
-                                    Number(spendingLimit.amount.value),
-                                    spendingLimit.amount.currency,
-                                  ),
-                                  period: spendingLimit.period,
-                                  day: translateDay(mode.startWeekDay, locale.language),
-                                  hour: mode.startHour,
-                                })}
-                              </LakeText>
-                            ))
-                            .otherwise(() => null);
 
                         return (
                           <>
@@ -1214,49 +1167,11 @@ export const CardItemPhysicalDetails = ({
 
                               <Space height={8} />
 
-                              {isNotNullish(spendingLimit.mode) ? (
-                                <View style={styles.spendingLimitText}>
-                                  {getSpendingLimitCalendar(spendingLimit.mode)}
-                                  <Fill minWidth={24} />
-
-                                  <LakeText
-                                    color={
-                                      hasBindingUserError
-                                        ? colors.gray[300]
-                                        : Number(spending.amount.value) >=
-                                            Number(spendingLimit.amount.value)
-                                          ? colors.negative[500]
-                                          : colors.gray[800]
-                                    }
-                                    variant="smallSemibold"
-                                  >
-                                    {formatCurrency(remainderToSpend, spending.amount.currency)}
-                                  </LakeText>
-                                </View>
-                              ) : (
-                                <View style={styles.spendingLimitText}>
-                                  <LakeText color={textColor} variant="smallRegular">
-                                    {match(spendingLimit.period)
-                                      .with("Daily", () => t("card.spendingLimit.remaining.daily"))
-                                      .with("Weekly", () =>
-                                        t("card.spendingLimit.remaining.weekly"),
-                                      )
-                                      .with("Monthly", () =>
-                                        t("card.spendingLimit.remaining.monthly"),
-                                      )
-                                      .with("Always", () =>
-                                        t("card.spendingLimit.remaining.always"),
-                                      )
-                                      .exhaustive()}
-                                  </LakeText>
-
-                                  <Fill minWidth={24} />
-
-                                  <LakeText color={textColor} variant="smallRegular">
-                                    {formatCurrency(remainderToSpend, spending.amount.currency)}
-                                  </LakeText>
-                                </View>
-                              )}
+                              <RemainingSpendingLimit
+                                spending={spending}
+                                spendingLimit={spendingLimit}
+                                hasBindingUserError={hasBindingUserError}
+                              />
 
                               <Space height={24} />
                             </View>

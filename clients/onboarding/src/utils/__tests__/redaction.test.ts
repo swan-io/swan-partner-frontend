c38bd89594b5fe5ac79480eff5cceaf5f02923b5
Thibaut Baillet <thibaut.baillet@swan.io>
2026-09-10 14:24:15 +0200

    Fix lint

--- clients/onboarding/src/utils/__tests__/redaction.test.ts
+++ clients/onboarding/src/utils/__tests__/redaction.test.ts
@@ -4,7 +4,6 @@ import { hashIdentifier, sanitizePathname, sanitizeProperties, sanitizeUrl } fro
 
 const ONBOARDING_ID = "3f2b8c1a-4d5e-4f60-9a7b-1c2d3e4f5a6b";
 const COLLECTION_ID = "9E4B7C2D-1A3F-4B5C-8D6E-7F809A1B2C3D";
-const UUID_SHAPE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 test("sanitizePathname replaces identifier segments", () => {
   expect(sanitizePathname(`/onboardings/${ONBOARDING_ID}/documents`)).toBe(
